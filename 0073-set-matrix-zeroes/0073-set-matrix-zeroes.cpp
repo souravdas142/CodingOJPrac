25e15461fe85d3d990143612af84a44f8e2c588a
Sourav Das <souravdas142@gmail.com>
2024-08-10 09:12:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-
+// Bruteforce
         int row = matrix.size();
         int col = matrix[0].size();
-        vector<vector<int> > matrix2 = matrix;
-
         for(int i = 0;i<row;i++) {
-            for(int j = 0;j<col;j++) {
-                if(matrix2[i][j]==0) {
+            for(int j= 0;j<col;j++) {
+                // O(n^2)
+                if(matrix[i][j]==0) {
+                    
                     for(int k = 0;k<col;k++) {
-                        matrix[i][k] = 0;
+                        if(matrix[i][k]==0) continue;
+                        matrix[i][k] = -1;
                     }
                     for(int k = 0;k<row;k++) {
-                        matrix[k][j] = 0;
+                        if(matrix[k][j]==0) continue;
+                        matrix[k][j] = -1;
                     }
-                    
                 }
             }
         }
-        
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                if(matrix[i][j]==-1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
