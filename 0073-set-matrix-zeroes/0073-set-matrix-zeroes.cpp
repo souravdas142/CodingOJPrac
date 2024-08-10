932199796ee33a0304f7449c6bc9a1b3e2748a87
Sourav Das <souravdas142@gmail.com>
2024-08-10 10:40:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,22 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
-        vector<int> rowArr(row,0);
-        vector<int> colArr(col,0);
+        int col0 = 1;
         for(int i = 0;i<row;i++) {
             for(int j = 0;j<col;j++) {
                 if(matrix[i][j]==0) {
-                    rowArr[i]=1;
-                    colArr[j]=1;
+                    matrix[i][0]=0;
+                    if(j == 0) col0 = 0;
+                    else matrix[0][j]=0;
                 }
             }
         }
 
-        for(int i= 0;i<row;i++) {
-            for(int j = 0;j<col;j++) {
-                if(rowArr[i]==1 || colArr[j]==1) {
+        for(int i= 1;i<row;i++) {
+            for(int j = 1;j<col;j++) {
+                if(matrix[i][0]==0 || matrix[0][j]==0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        if(matrix[0][0]==0) {
+            for(int i = 0;i<row;i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if(col0 == 0) {
+            for(int i = 0;i<col;i++) {
+                matrix[0][i] = 0;
             }
         }
     }
