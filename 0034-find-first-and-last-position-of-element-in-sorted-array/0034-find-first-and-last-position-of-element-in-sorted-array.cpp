c2d829c171a3b8b5d44bbf7ba33f5df8fa9f23c5
Sourav Das <souravdas142@gmail.com>
2024-08-15 15:01:51 +0530

    Time: 10 ms (17.99%), Space: 16.3 MB (47.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,55 +1,45 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-       int right = nums.size()-1;
-       int left = 0;
-       int firstpos = -1;
-       int firstEnc = -1;
-
-       while(left<=right) {
-            int mid = left+(right-left)/2;
-            if(nums[mid]==target) { // we found but we may have anoter target on left
-                right = mid-1;
-                firstpos = mid;
-                if(firstEnc==-1) firstEnc = mid;
-
-            }
-            else if(nums[mid]>target) {
-                right = mid-1;
-            }
-            else if(nums[mid]<target) {
-                left = mid+1;
-            }
-       } 
-
-        if(firstEnc==-1) return {firstpos,firstpos};
-        cout<<firstEnc<<endl;
-
-        left = firstEnc;
-        right = nums.size()-1;
-        int sec = -1;
-
+    int lb(vector<int>& nums,int target) {
+        int sz = nums.size();
+        int left = 0, right = sz-1;
         while(left<=right) {
             int mid = left+(right-left)/2;
-
-            if(nums[mid]==target) { // we may have another in right
-                sec = mid;
-                left = mid+1;
-            }
-            else if(nums[mid]>target) {
+            if(nums[mid]>=target) {
                 right = mid-1;
             }
-            else if(nums[mid]<target) {
+            else {
                 left = mid+1;
             }
         }
+        if(left <sz && left>=0 && nums[left] == target) 
+            return left;
+        return -1;
+    }
+    int up(vector<int>& nums,int target) {
+        int sz = nums.size();
+        int left = 0, right = sz-1;
+        while(left<=right) {
+            int mid = left+(right-left)/2;
+            if(nums[mid]>target) {
+                right = mid-1;
+            }
+            else {
+                left = mid+1;
+            }
+        }
+        if(left-1>=0 && left-1<sz && nums[left-1]==target)
+            return left-1;
+        
+        return -1;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
 
-        if(firstpos==-1 && sec == -1) return {-1,-1};
-        if(sec==-1) return {firstpos,firstpos};
-        return {firstpos,sec};
+        if(nums.size() == 1 && nums[0]==target) return {0,0};
+        int lbv = lb(nums,target);
+        int upv = up(nums,target);
 
-
-        //return {firstpos,-1};
-
+        return {lbv,upv};
+        
     }
 };
