ef45cd20362f19cf28ad46698b7a59ed5879f8a5
Sourav Das <souravdas142@gmail.com>
2024-08-18 11:37:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,21 +1,35 @@
 class Solution {
+private:
+    int lbf(vector<int>& arr,int x) {
+        int sz = arr.size();
+        int left = 0;int right = sz-1;
+        while(left<right) {
+            int mid = left+(right-left)/2;
+            if(arr[mid]>=x) {
+                right = mid;
+            }
+            else {
+                left = mid+1;
+            }
+            
+        }
+        return left;
+    }
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // write your code here
+        int lb = lbf(arr,x);
         int sz = arr.size();
         vector<int> ans;
-        auto lb = lower_bound(arr.begin(),arr.end(),x);
-        if(lb == arr.end() || lb == arr.end()-1) {
-            ans = vector<int>(arr.begin()+(sz-k),arr.end());
-            return ans;
-        }
-        int j = lb-arr.begin();
-        int i = j-1;
-
-        while(k>0 && i>= 0 && j<sz) {
-            int m = abs(arr[j]-x);
-            int n = abs(arr[i]-x);
-
-            if(m<n) {
+        //ans.push_back(arr[lb]);
+        int i = lb - 1,j = lb;
+        //k--;
+        while(i>=0 && j<sz && k>0) {
+            if(arr[i]<arr[j]) {
+                ans.push_back(arr[i]);
+                i--;
+            }
+            else if(arr[j]<arr[i]) {
                 ans.push_back(arr[j]);
                 j++;
             }
@@ -24,18 +38,16 @@ public:
                 i--;
             }
             k--;
-        }
-        while(k>0 && i>=0) {
-            ans.push_back(arr[i]);
-            i--;
-            k--;
-        }
-        while(k>0 &&j<sz) {
-            ans.push_back(arr[j]);
-            j++;
-            k--;
-        }
 
+        } 
+        while(k>0 && i>=0) {
+            ans.push_back(arr[i--]);
+            k--;
+        }
+        while(k>0 && j<sz) {
+            ans.push_back(arr[j++]);
+            k--;
+        }
         sort(ans.begin(),ans.end());
         return ans;
     }
