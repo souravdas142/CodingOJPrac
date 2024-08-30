ad19281a9a30e71b4b6b4f21cb9302eacc2ef808
Sourav Das <souravdas142@gmail.com>
2024-08-30 23:50:14 +0530

    Time: 9 ms (98.08%), Space: 16.7 MB (23.68%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,48 +1,44 @@
 class Solution {
 
-    bool bfs(vector<vector<int> >& adjlist,vector<int>& isColored,int node) {
+    bool bfs(vector<vector<int> >& adjlist, vector<int>& isColored,int source) {
 
-        isColored[node] = 0;
-        deque<pair<int,int> > dq;
-        dq.push_back({node,-1});
+        isColored[source]  = 0;
+        deque<int> dq;
+        dq.push_back(source);
 
         while(!dq.empty()) {
-
-            pair<int,int> u = dq.front();
+            int u = dq.front();
             dq.pop_front();
-            int sz = adjlist[u.first].size();
 
-            for(int i = 0;i<sz;i++) {
+            int n = adjlist[u].size();
+            for(int i = 0;i<n;i++) {
                 
-                int v = adjlist[u.first][i];
-                int newColor = 1-isColored[u.first];
+                int v = adjlist[u][i];
+                int newColor = 1-isColored[u];
                 if(isColored[v]==-1) {
-                    dq.push_back({v,u.first});
                     isColored[v] = newColor;
-                }   
-                else if(u.second!=v) {
-                    if(isColored[v]!=newColor) {
+                    dq.push_back(v);
+                }
+                else {
+                    if(newColor != isColored[v] ) {
                         return false;
                     }
                 }
-
             }
-
         }
         return true;
     }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-
-        int n = graph.size();
-        vector<int> isColored(n,-1);
-
-        for(int i = 0;i<n;i++) {
+       int n = graph.size();
+       vector<int> isColored(n,-1);
+       for(int i= 0;i<n;i++ ) {
             if(isColored[i]==-1) {
                 if(bfs(graph,isColored,i)==false) return false;
             }
-        }
-        
-        return true;
+       } 
+
+       return true;
     }
 };
