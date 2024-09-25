dd6281887cd654104463c5c4793e7ca51c19d76a
Sourav Das <souravdas142@gmail.com>
2024-09-25 22:31:15 +0530

    Time: 15 ms (78.3%), Space: 10.1 MB (42.65%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -8,11 +8,10 @@ public:
         int ans = 0;
         while(j<n) {
             if(s[j]-'a' >=0 && s[j]-'a'<3) mp[s[j]-'a']++;
-            if(mp[0]>=1 && mp[1]>=1 && mp[2]>=1) {
-                ans+=(n-1-j);
-            }
+            
             while(mp[0]>=1 && mp[1]>=1 && mp[2]>=1) {
-                ans++;
+                
+                ans+=(n-j);
                 if(s[i]-'a' >=0 && s[i]-'a'<3) mp[s[i]-'a']--;
                 i++;
 
