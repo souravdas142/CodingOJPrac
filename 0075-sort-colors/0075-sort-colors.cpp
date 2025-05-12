1f82fb8b3ce87cabc2e6d8483f81b609cb896865
Sourav Das <souravdas142@gmail.com>
2025-05-12 23:56:11 +0530

    Time: 2 ms (3.59%), Space: 9.9 MB (100%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,52 +1,29 @@
 class Solution {
-    void merge(vector<int>& nums,int sz,int left, int mid, int right) {
-        vector<int> tmp;
-        int lend = mid;
-        int rf = mid+1;
-        int a = left;
-        int i = 0;
-        while(left<=lend && rf<=right) {
-            if(nums[left]>nums[rf]) {
-                tmp.push_back(nums[rf]);
-                rf++;
-            }
-            else if(nums[left]<=nums[rf]) {
-                tmp.push_back(nums[left++]);
-            }
-            i++;
-            
-        }
-        while(left<=lend) {
-            tmp.push_back(nums[left++]);
-            
-        }
-        while(rf<=right) {
-            tmp.push_back(nums[rf++]);
-
-        }
-        i = a;
-        int k = 0;
-        while(i<=right) {
-            nums[i] = tmp[k++];
-            i++;
-        }
-    }
-private:
-    void msort(vector<int>& nums,int sz,int left,int right) {
-        if(left>=right) return;
-
-        int mid = left+(right-left)/2;
-        msort(nums,mid-left+1,left,mid);
-        msort(nums,right-mid,mid+1,right);
-        merge(nums,sz,left,mid,right);
-    }
 public:
     void sortColors(vector<int>& nums) {
-
+        int cntz = 0,cnt1 = 0, cnt2 = 0;
         int sz = nums.size();
-        int left = 0,right  =sz - 1;
-        msort(nums,sz,left,right);
-        
+
+        for(int i= 0;i<sz;i++) {
+            if(nums[i]==0) cntz++;
+            else if(nums[i]==1) cnt1++;
+            else if(nums[i]==2) cnt2++;
+        }
+
+        for(int i= 0;i<sz;i++) {
+            if(cntz>0) {
+                nums[i]=0;
+                cntz--;
+            }
+            else if(cnt1>0) {
+                nums[i]=1;
+                cnt1--;
+            }
+            else if(cnt2>0) {
+                nums[i]  = 2;
+                cnt2--;
+            }
+        }
         
     }
 };
