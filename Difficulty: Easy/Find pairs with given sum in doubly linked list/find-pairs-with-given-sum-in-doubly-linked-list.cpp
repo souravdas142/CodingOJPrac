26d14a88026ed6b284f9431060ec387092d6d047
Sourav Das <souravdas142@gmail.com>
2025-05-15 20:25:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target) {
+        // code here
+        Node* cur = head;
+        vector<pair<int,int> > ans;
+        unordered_map<int,bool> look;
+        while(cur) {
+            int data = cur->data;
+            int need = target - data;
+            if(look.find(need)!=look.end()) {
+                ans.push_back({need,data});
+            }
+            look[data] = true;
+            cur = cur->next;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, target;
+        cin >> target >> n;
+        int a;
+        cin >> a;
+        Node *head = new Node(a);
+        Node *tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> a;
+            tail->next = new Node(a);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+        Solution ob;
+        auto ans = ob.findPairsWithGivenSum(head, target);
+        if (ans.size() == 0)
+            cout << "-1";
+        else
+        {
+            for (int i = 0; i < ans.size(); i++)
+            {
+                cout << "(" << ans[i].first << "," << ans[i].second << ")"
+                     << " ";
+            }
+        }
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
