f6fa99d420c29a3573a7115208af11dd3479d839
Sourav Das <souravdas142@gmail.com>
2025-05-17 22:31:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -1,60 +1,72 @@
 //{ Driver Code Starts
-// A C++ program to find if there is a zero sum
-// subarray
 #include <bits/stdc++.h>
 using namespace std;
 
 
-
 // } Driver Code Ends
-class Solution{
-    public:
-    //Complete this function
-    //Function to check whether there is a subarray present with 0-sum or not.
-    bool subArrayExists(int arr[], int n)
-    {
-        //Your code here
-        unordered_map<int,int> ump; // prefixSum, index
+
+class Solution {
+  public:
+    // Complete this function
+    // Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(vector<int>& arr) {
+        // Your code here
+        int n = arr.size();
         int i = 0;
         int sum = 0;
-       
+        int target = 0;
+        unordered_map<int,int> look;
+        
         while(i<n) {
-            sum += arr[i];
-            if(sum==0) return true;
-            int need = sum;
-            if(ump.count(need)==1) {
+            sum+=arr[i];
+            if(sum==target) {
                 return true;
             }
             
-            if(ump.count(sum)==0)
-                ump[sum] = i;
+            int need = sum - target;
+            if(look.find(need)!=look.end()) {
+                return true;
+            }
             
+            if(look.find(sum)==look.end()) {
+                look.insert({sum,i});
+            }
             
             i++;
+            
+            
         }
+        
         return false;
     }
 };
 
+
 //{ Driver Code Starts.
-// Driver code
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++)
-	    cin>>arr[i];
-	    Solution obj;
-	    	if (obj.subArrayExists(arr, n))
-		cout << "Yes\n";
-	else
-		cout << "No\n";
-	}
-	return 0;
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        if (obj.subArrayExists(arr))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~" << endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
