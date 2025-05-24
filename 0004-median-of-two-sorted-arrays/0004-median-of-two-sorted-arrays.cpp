16ae62625a2b4088df7eb4179511c3284ea4dca1
Sourav Das <souravdas142@gmail.com>
2025-05-24 18:32:08 +0530

    Time: 19 ms (5.04%), Space: 94.5 MB (99.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,7 +5,7 @@ public:
         int n1=nums1.size();
         int n2 = nums2.size();
 
-        if(n2>n1) {
+        if(n2<n1) {
             return findMedianSortedArrays(nums2,nums1);
         }
 
@@ -17,13 +17,13 @@ public:
 
             int mid1 = (high+low)/2;
             int mid2 = left-mid1;
-            int l1 = (mid1>0)?nums1[mid1-1]:INT_MIN, r1 = (mid1<n1)?nums1[mid1]:INT_MAX;
-            int l2 = (mid2>0)?nums2[mid2-1]:INT_MIN, r2 = (mid2<n2)?nums2[mid2]:INT_MAX;
+            int l1 = (mid1-1>=0)?nums1[mid1-1]:INT_MIN, r1 = (mid1<n1)?nums1[mid1]:INT_MAX;
+            int l2 = (mid2-1>=0)?nums2[mid2-1]:INT_MIN, r2 = (mid2<n2)?nums2[mid2]:INT_MAX;
             
             if(l1<=r2 && l2<=r1) {
 
                 if(n&1) {
-                    return (max(l1,l2))/1.0;
+                    return max(l1,l2);
                 }
                 else {
                     return (max(l1,l2)+min(r1,r2))/2.0;
