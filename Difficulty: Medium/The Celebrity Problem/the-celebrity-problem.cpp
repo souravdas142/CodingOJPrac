e9784390c73193452425a3c353c88fdc1135c40c
Sourav Das <souravdas142@gmail.com>
2025-05-25 11:27:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,73 +1,26 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& mat) {
         // code here
-        stack<int> st;
-        int n = mat.size();
-        for(int i = 0;i<n;i++) {
-            st.push(i);
-        }
-        
-        while(st.size()>1) {
-            int i = st.top();
-            st.pop();
-            int j = st.top();
-            st.pop();
-            
-            if(mat[i][j] && mat[j][i]) {
-                continue;
+        int row = mat.size();
+        int col = mat[0].size();
+        for(int i = 0;i<row;i++) {
+            int sum = 0;
+            for(int j = 0;j<col;j++) {
+                sum+=mat[i][j];
             }
-            if(mat[i][j]) { // if i knows j ten it shouldn't be celibrity
-                
-                st.push(j);  /// may be j is celibrity?
-                
+            if(sum==1) { // may be cleibrity
+                int is_celeb = 1;
+                for(int k = 0;k<row;k++) {
+                    if(k==i) continue;
+                    if(mat[k][i]!=1) {
+                        is_celeb = 0;
+                        break;
+                    }
+                }
+                if(is_celeb==1) return i;
             }
-            if(mat[j][i]) { //if j knows i, j is not celibrity
-                st.push(i);
-            }
-            
-            
-            
         }
-        if(st.empty()) return -1;
-        int tp = st.top();
-        for(int k = 0;k<n;k++) {
-            if(tp==k) continue;
-            if(mat[tp][k]!=0) return -1;
-            if(mat[k][tp]!=1) return -1;
-        }
-        return tp;
+        return -1;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-    }
-}
-
-// } Driver Code Ends
