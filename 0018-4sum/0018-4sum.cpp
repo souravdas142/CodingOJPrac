b2e36e632dfd1aaa72df80fc78dfa59fedc63239
Sourav Das <souravdas142@gmail.com>
2025-05-25 15:14:25 +0530

    Time: 26 ms (33.6%), Space: 17.5 MB (58.66%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,46 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-       sort(nums.begin(),nums.end());
-    vector<vector<int> > ans;
-    int sz = nums.size();
-    int i = 0,j = 1, k = 2, l = sz-1;
-    for(int i = 0;i<sz;) {
-        
-        for(int j = i+1;j<sz;) {
 
-            int k = j+1,l = sz-1;
-            
-            while(k<l) {
-            
-                long long sum = nums[i]+nums[j];
-                sum+=nums[k]+nums[l];
+        int n = nums.size();
+        int i = 0,j = 0,k = 0,l = n-1;
+        
+        sort(nums.begin(),nums.end());
+        vector<vector<int> > ans;
+
+        while(i<n) {
+            j=i+1;
+            while(j<n) {
+                k = j+1;
                 
-                if(sum == target) {
-                  vector<int> tmp = {nums[i], nums[j], nums[k], nums[l]};
-                  ans.push_back(tmp);
-                  k++;l--;
-                  while(k<l && nums[k]==nums[k-1]) k++;
-                  while(k<l && nums[l]==nums[l+1]) l--;
-                }
-                else if(sum<target) {
-                    k++;
-                    while(k<l && nums[k]==nums[k-1]) k++;
-                    
-                }
-                else if(sum>target) {
-                    l--;
-                    while(k<l && nums[l]==nums[l+1]) l--;
-                }
-            }
-            j++;
-            while(j<sz && nums[j]==nums[j-1]) j++;
-        }
-        i++;
-        while(i<sz && nums[i]==nums[i-1]) i++;
-        
-    }
+                
+                l = n-1;
 
-    return ans; 
+                while(k<l) {
+                    long long tsum = nums[i]+nums[j];
+                    tsum+=(nums[k]+nums[l]);
+                  
+
+                    if(tsum == target) {
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(tsum<target) {
+                        k++;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                    
+                    }
+                    else if(tsum>target) {
+                        l--;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    
+                   
+                    
+
+                }
+
+                j++;
+                while(j<n && nums[j]==nums[j-1]) j++;
+            }
+            i++;
+            while(i<n && nums[i]==nums[i-1]) i++;
+        }
+        return ans;
     }
 };
