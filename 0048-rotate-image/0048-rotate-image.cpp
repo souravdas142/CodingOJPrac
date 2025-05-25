250e9cb77b9d361b2f6c6f0013d00f82c218fea2
Sourav Das <souravdas142@gmail.com>
2025-05-25 10:42:26 +0530

    Time: 0 ms (100%), Space: 10.1 MB (91.95%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,37 @@
+
+ostream& operator<<(ostream& print, vector<vector<int> >& mat) {
+    for(auto vvec:mat) {
+        for(auto vec:vvec ) {
+            print<<vec<<" ";
+        }
+        print<<"\n";
+    }
+    return print;
+}
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-
-        int sz  = matrix.size();
-        for(int i = 0;i<sz-1;i++) {
-            for(int j = i+1;j<sz;j++) {
-                swap(matrix[j][i],matrix[i][j]);
+        //cout<<"original\n"<<matrix<<endl;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        for(int i = 0;i<row;i++) {
+            for(int j = i+1;j<col;j++) {
+                if(i==j) continue;
+                swap(matrix[i][j],matrix[j][i]);
+                //cout<<matrix<<endl;
             }
         }
-
-        for(int i= 0;i<sz;i++) {
-            int j = sz- 1,k= 0;
-            while(k<=j) {
-                swap(matrix[i][k],matrix[i][j]);
-                k++;j--;
+        //cout<<endl;
+        //cout<<"Transpose\n"<<matrix<<endl<<endl;
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                int revcol = col-1-j;
+                if(j>=revcol) break;
+                swap(matrix[i][j],matrix[i][revcol]);
             }
         }
-        
+        //cout<<endl;
+        //cout<<"90 rotate\n"<<matrix<<endl<<endl;
     }
 };
