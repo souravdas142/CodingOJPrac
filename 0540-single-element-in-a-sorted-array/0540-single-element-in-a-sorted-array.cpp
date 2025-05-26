f4cd4b3cc30c824240ea7348c341dc17c401bacf
Sourav Das <souravdas142@gmail.com>
2025-05-27 01:27:45 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -18,17 +18,10 @@ public:
             if(mid&1) {
                 if(nums[mid]==nums[mid+1])
                     high = mid-1;
-                else if(nums[mid]==nums[mid-1])
+                else 
                     low = mid+1;
-                //else return nums[mid];
-            }/*
-            else {
-                if(nums[mid]==nums[mid+1])
-                    low = mid+2;
-                else if(nums[mid]==nums[mid-1])
-                    high = mid-2;
-                else return nums[mid];
-            }*/
+               
+            }
         }
         return nums[high];
     }   
