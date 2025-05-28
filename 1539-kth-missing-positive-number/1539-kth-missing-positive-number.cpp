ddefa61cd9743311639d2851f87efadadbe69e64
Sourav Das <souravdas142@gmail.com>
2025-05-29 01:58:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
-        int i = 1;
+        int i = 0;
         int j = 0;
         int cnt = 0;
+        int ans = 0;
 
         while(j<n) {
-            if(i == arr[j]) {
-                i++;j++;
-            }
-            else {
-                cnt++;
-                if(cnt == k) {
-                    cnt = i;
-                    break;
+            int m = arr[j]-i-1;
+
+           
+                if(cnt+m>=k) {
+                    ans = (i+(k-cnt));
+                    
                 }
-                i++;
-            }
+                cnt+=m;
+            
+            if(cnt==k) break;
+            i = arr[j];
+
+
+            j++;
         }
         if(k>cnt) {
-            k = k-cnt;
-            return (arr[n-1]+k);
+            return (i+(k-cnt));
         }
-        return cnt;
+        
+        return ans;
         
     }
 };
