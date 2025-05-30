78862fce24ba05626d08d2b44df90bc19fd469ab
Sourav Das <souravdas142@gmail.com>
2025-05-31 01:33:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -2,36 +2,36 @@ class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
         // code here
-        int i = 0;
-        int j = 0;
-        int cnt = 0;
-        int cur = 0;
-        while(i<a.size() && j<b.size()) {
-            if(a[i]<=b[j]) {
-                cur=a[i];
-                i++;
-            }    
+        int n = a.size();
+        int m = b.size();
+        if(n>m) return kthElement(b,a,k);
+        
+        int low = max(0,k-m), high = min(k,n);
+        
+        int med = k;
+        
+        while(low<=high) {
+            int mid1 = low+(high-low)/2;
+            int mid2 = med-mid1;
+            
+            int l1 = (mid1-1<0)?INT_MIN:a[mid1-1];
+            int l2 = (mid2-1<0)?INT_MIN:b[mid2-1];
+            int r1 = (mid1<n)?a[mid1]:INT_MAX;
+            int r2 = (mid2<m)?b[mid2]:INT_MAX;
+            
+            if(l1>r2) {
+                high = mid1-1;
+            }
+            else if(l2>r1) {
+                low = mid1+1;
+            }
             else {
-                cur = b[j];
-                j++;
+                return max(l1,l2);
             }
-            cnt++;
-            if(cnt == k) {
-                return cur;
-            }
+            
+            
         }
-        while(i<a.size()) {
-            cur = a[i];
-            i++;
-            cnt++;
-            if(cnt == k) return cur;
-        }
-        while(j<b.size()) {
-            cur = b[j];
-            j++;
-            cnt++;
-            if(cnt==k) return cur;
-        }
-        return cur;
-     }
+        return 0;
+        
+    }
 };
