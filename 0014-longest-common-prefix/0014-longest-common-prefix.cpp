ff0ef7b822a0d9dd3401c4ca318717d363cf5887
Sourav Das <souravdas142@gmail.com>
2025-05-31 17:45:42 +0530

    Time: 3 ms (13.25%), Space: 11.9 MB (49.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,20 +1,55 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-
-        sort(strs.begin(),strs.end());
-        string ans = "";
-        string a = strs[0];
-        string b = strs.back();
-        int alen = a.length();
-        int blen = b.length();
-        int i = 0,j = 0;
-        while(i<alen && j<blen) {
-            if(a[i]==b[j]) ans+=a[i];
-            else break;
-            i++;j++;
-        }
-        return ans;
         
+        int sz = strs.size();
+        if(sz==1) return strs[0];
+        int mn = 201;
+        string s;
+        int pos = -1;
+        for(int i= 0;i<sz;i++) {
+            int len = strs[i].length();
+            if(len<mn) {
+                mn = len;
+                s = strs[i];
+                pos = i;
+            }
+        }
+        if(mn==0) return "";
+        unordered_map<string,int> ump;
+        string str;
+        for(int i = 0;i<mn;i++) {
+            str+=s[i];
+            ump[str]=i+1;
+            
+        }
+
+        int minlen = 0;
+        string strlen=s;
+        for(int i = 0;i<sz;i++) {
+            string eachs = strs[i];
+            if(i==pos) continue;
+            int eachsz = eachs.length();
+            string ls;
+            for(int j = 0;j<eachsz;j++) {
+                ls+=eachs[j];
+                if(!ump.count(ls)){
+                    ls.pop_back();
+                    break;
+                }
+
+            }
+
+            int lssz = ls.size();
+            if(lssz == 0) return "";
+            if(lssz<strlen.size()) {
+                strlen=ls;
+            }
+
+
+        }
+
+
+        return strlen;
     }
 };
