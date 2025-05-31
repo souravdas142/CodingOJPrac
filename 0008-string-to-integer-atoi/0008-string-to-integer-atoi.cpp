e2a6e0e45ed5ed585c948b1c59f232a3c7233540
Sourav Das <souravdas142@gmail.com>
2025-06-01 00:39:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int myAtoi(string s) {
 
-        // ignore leading whitespace and leading zeros
-        int len = s.length();
-        int i = 0; int sign = 1;
-        while(i<len && s[i]==' ') i++;
-        if(i<len && s[i]=='-') {
-            sign = -1;
+        int is_neg = 1;
+
+        int i = 0;
+        int n = s.length();
+
+        while(s[i]==' ') {
             i++;
         }
-        else if(i<len && s[i]=='+') {
+        //cout<<i<<endl;
+        if(s[i]=='-' || s[i]=='+') {
+            if(s[i]=='-')
+                is_neg = -1;
             i++;
         }
-        while(i<len && s[i]=='0') i++;
-        int j = i;
-        long ans = 0;
-        cout<<i<<endl;
-        while(i<len && s[i]>='0' && s[i]<='9') {
+        
+        //cout<<i<<is_neg<<endl;
+        long long num = 0;
 
-            ans=((ans*10)+(s[i]-'0'));
-            cout<<ans<<endl;
-            if(ans>INT_MAX && sign==-1) return INT_MIN;
-            else if(ans>INT_MAX && sign==1) return INT_MAX;
-
+        while(s[i]>='0' && s[i]<='9') {
+            num = (num*10)+(s[i]-'0');
+            if(num*is_neg>INT_MAX) return INT_MAX;
+            if(num*is_neg<INT_MIN) return INT_MIN;
+            //cout<<"num = "<<num<<endl;
             i++;
         }
-
-        return (ans*sign);
 
         
 
-
+        return (num*is_neg);
         
     }
 };
