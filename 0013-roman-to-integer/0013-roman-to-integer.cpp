fe1f1792997382f183958f6b3a2a665fe8815def
Sourav Das <souravdas142@gmail.com>
2025-05-31 23:06:56 +0530

    Time: 31 ms (5.48%), Space: 12.7 MB (54.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        map<char,int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
 
-        unordered_map<char,int> roman = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        long long result  = mp[s[0]];
+        cout<<"result = "<<result<<"\n";
+        for(int i = 1;i<s.length();i++) {
+                result += mp[s[i]];
+                if((s[i]=='V' && s[i-1]=='I') || (s[i]=='X' && s[i-1]=='I')) {
+                    result = result-mp[s[i-1]]-mp[s[i]];
+                    if(s[i]=='V') result+=4;
+                    if(s[i]=='X') result+=9;
+                }
+                else if((s[i]=='L' && s[i-1]=='X') || (s[i]=='C' && s[i-1]=='X')) {
+                    result = result-mp[s[i-1]]-mp[s[i]];
+                    if(s[i]=='L') result+=40;
+                    if(s[i]=='C') result+=90;
+                }
+                else if((s[i]=='D' && s[i-1]=='C') || (s[i]=='M' && s[i-1]=='C')) {
+                    result = result-mp[s[i-1]]-mp[s[i]];
+                    if(s[i]=='D') result+=400;
+                    if(s[i]=='M') result+=900;
+                }
+                cout<<"result = "<<result<<"\n";
 
-        int n = s.length();
-        int i = 0;
-        int num = 0;
-
-        while(i<n-1) {
-            char r = s[i];
-            char r1 = s[i+1];
-            if(roman[r]>=roman[r1]) {
-                num+=roman[r];
-            }
-            else {
-                int k = roman[r1]-roman[r];
-                num+=k;
-                i++;
-            }
-
-
-
-            i++;
         }
-        char r = s[i-1];
-        char r1 = s[i];
+        return result;
 
-        if(roman[r]>=roman[r1]) {
-            num+=roman[r1];
-        }
-        
-
-        return num;
-
-        
         
     }
 };
