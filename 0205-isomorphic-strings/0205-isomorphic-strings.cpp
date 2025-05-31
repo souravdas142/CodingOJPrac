9d306db58ff7a8be0eb4101dd7e1dc7593433f3e
Sourav Das <souravdas142@gmail.com>
2025-05-31 18:43:32 +0530

    Time: 2 ms (51.65%), Space: 9.5 MB (9.37%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,set<char> > ump,ump2;
-        int slen = s.length(), tlen = t.length();
-        if(slen!=tlen) return false;
 
-        for(int i = 0;i<slen;i++) {
-            ump[s[i]].insert(t[i]);
-            ump2[t[i]].insert(s[i]);
-            /// not sure??
+        unordered_map<char,char> ump1, ump2;
+        int n = s.length(), m = t.length();
+        if(n!=m) return false;
+
+        for(int i = 0;i<n;i++) {
+
+            if(ump1.find(s[i])==ump1.end()) {
+                ump1[s[i]] = t[i];
+            }
+            else {
+                if(ump1[s[i]]!=t[i]) return false;
+            }
+            if(ump2.find(t[i])==ump2.end()) {
+                ump2[t[i]] = s[i];
+            }
+            else {
+                if(ump2[t[i]]!=s[i])
+                    return false;
+            }
         }
 
-        for(pair<char,set<char> > s:ump) {
-            if(s.second.size()>1) return false;
-        }
-        for(pair<char,set<char> > s:ump2) {
-            if(s.second.size()>1) return false;
-        }
-
+        
         return true;
-
         
     }
 };
