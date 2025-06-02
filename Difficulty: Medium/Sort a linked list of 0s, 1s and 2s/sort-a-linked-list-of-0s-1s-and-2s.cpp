c69e6198cbf126e56ae1e38d0da04b56ea2a22fc
Sourav Das <souravdas142@gmail.com>
2025-06-03 01:37:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -18,36 +18,36 @@ class Solution {
     
    void swapNodes(int i, int j, vector<Node*>& nodes) {
     if (i == j) return;
+    
+    if(i>j) swap(i,j);
 
-    if (i > j) swap(i, j); // ensure i < j for consistency
-
-    Node* prev_i = nodes[i - 1];
-    Node* node_i = nodes[i];
-    Node* prev_j = nodes[j - 1];
-    Node* node_j = nodes[j];
-
-    if (j == i + 1) {
-        // Adjacent nodes
-        prev_i->next = node_j;
-        node_i->next = node_j->next;
-        node_j->next = node_i;
-    } else {
-        Node* temp = node_j->next;
-
-        prev_i->next = node_j;
-        node_j->next = node_i->next;
-
-        prev_j->next = node_i;
-        node_i->next = temp;
+    Node* prev_i = nodes[i-1];
+    Node* cur_i = nodes[i];
+    Node* prev_j = nodes[j-1];
+    Node* cur_j = nodes[j];
+    
+    if(i+1==j) { // handle adjacent nodes
+        prev_i->next = cur_j;
+        cur_i->next = cur_j->next;
+        cur_j->next = cur_i;
+        
     }
-
-    swap(nodes[i], nodes[j]);
+    else {
+        Node* tmp =cur_j->next;
+        prev_i->next = cur_j;
+        cur_j->next = cur_i->next;
+        prev_j->next = cur_i;
+        cur_i->next = tmp;
+    }
+    
+    swap(nodes[i],nodes[j]);
+    
 }
 
   
     Node* segregate(Node* head) {
         // code here
-        
+        /*
         Node* dummy = new Node(0);
         dummy->next = head;
         vector<Node*> nodes;
@@ -74,6 +74,45 @@ class Solution {
         }
         
         return nodes[0]->next;
+        */
+        
+        Node* zero = new Node(0);
+        Node* zeroi = zero;
+        Node* one = new Node(1);
+        Node* onei = one;
+        Node* two = new Node(2);
+        Node* twoi = two;
+        
+        Node* iter = head;
+        
+        while(iter!=nullptr) {
+            if(iter->data==0) {
+                zeroi->next = iter;
+                zeroi=zeroi->next;
+            }
+            else if(iter->data==1) {
+                onei->next = iter;
+                onei=onei->next;
+            }
+            else if(iter->data==2) {
+                twoi->next = iter;
+                twoi = twoi->next;
+            }
+            iter=iter->next;
+        }
+        
+        twoi->next = nullptr;
+        onei->next = two->next;
+        zeroi->next = one->next;
+        
+        
+        
+        return zero->next;
+        
+        
+        
+        
+        
         
     }
 };
