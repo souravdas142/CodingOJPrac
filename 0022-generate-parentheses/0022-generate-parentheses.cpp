3a3e1910cacf16e0480dec9f476fd784c5d60194
Sourav Das <souravdas142@gmail.com>
2025-06-08 11:36:08 +0530

    Time: 3 ms (71.86%), Space: 16.1 MB (26.24%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,55 +1,48 @@
 class Solution {
-public:
+    void solve(int depth,int left, int right,string res,vector<string>& ans) {
 
-    void solve(string& brackets,unordered_map<string,bool>& check, int n,int opn, int cls,vector<string>& ans, string res) {
+        
 
-        cout<<res<<endl;
-
-        if(res.size()>=n*2) {
-           
+        if(left==depth && left==right) {
+            
                 ans.push_back(res);
+                
+            
             return;
         }
 
-        if(cls>opn) return;
+        if(left>depth || right>depth) return;
 
-        if(opn<n && check.find(res+brackets[0])==check.end()) {
-            res.push_back(brackets[0]);
-            
-                opn++;
-                check[res] = true;
-                solve(brackets,check,n,opn,cls,ans,res);
-                opn--;
-            
-            res.pop_back();
-            //cout<<"opn "<<res<<endl;
-        } 
-        if(cls<n && check.find(res+brackets[1])==check.end()) {
-            
-            res+=brackets[1];
-            
-                cls++;
-                check[res] = true;
-                
-                solve(brackets,check,n,opn,cls,ans,res);
-                cls--;
-            
-            res.pop_back();
-            //cout<<res<<endl;
 
+        
+        /// take
+        if(left<depth) {
+            res.push_back('(');
+            solve(depth,left+1,right,res,ans);
+            res.pop_back();
+        }
+    
+
+        if(left>right && right<depth) {
+            res.push_back(')');
+            
+            solve(depth,left,right+1,res,ans);
+            res.pop_back();
         }
         
 
     }
+public:
     vector<string> generateParenthesis(int n) {
 
-        string brackets = "()";
+        int depth = n;
 
+        string res;
         vector<string> ans;
-        string res = "";
-        int opn = 0, cls = 0;
-        unordered_map<string,bool> check;
-        solve(brackets,check,n,opn, cls, ans,res);
+
+        int left = 0, right = 0;
+
+        solve(depth,left,right,res,ans);
 
         return ans;
         
