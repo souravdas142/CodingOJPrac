1cc10dbf55036202615f338747c13732a127f7ec
Sourav Das <souravdas142@gmail.com>
2025-06-09 14:50:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,37 +1,55 @@
+
+
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
-        // code here
         int n = a.size();
         int m = b.size();
         if(n>m) return kthElement(b,a,k);
+        int total = n+m;
         
+        
+        //int low = 0, high = k;
         int low = max(0,k-m), high = min(k,n);
-        
-        int med = k;
+        int l1 = -1;
+        int l2 = -1;
+        int r1 = -1;
+        int r2 = -1;
+        int ans = 0;
         
         while(low<=high) {
-            int mid1 = low+(high-low)/2;
-            int mid2 = med-mid1;
+            int mid = low+(high-low)/2;
+            r2 = k - mid;
+            r1 = mid;
+            l1 = r1-1;
             
-            int l1 = (mid1-1<0)?INT_MIN:a[mid1-1];
-            int l2 = (mid2-1<0)?INT_MIN:b[mid2-1];
-            int r1 = (mid1<n)?a[mid1]:INT_MAX;
-            int r2 = (mid2<m)?b[mid2]:INT_MAX;
+            l2 = r2-1;
             
-            if(l1>r2) {
-                high = mid1-1;
+            
+            int aleft = (l1<0)?INT_MIN:a[l1];
+            int bleft = (l2<0)?INT_MIN:b[l2];
+            int aright = (r1<n)?a[r1]:INT_MAX;
+            int bright = (r2<m)?b[r2]:INT_MAX;
+            
+            
+            
+            if(aleft>bright) {
+                high = mid-1;
             }
-            else if(l2>r1) {
-                low = mid1+1;
+            else if(bleft>aright) {
+                low = mid+1;
             }
+            
             else {
-                return max(l1,l2);
+                ans = max(aleft,bleft);
+                break;
             }
+            
             
             
         }
-        return 0;
+        
+        return ans;
         
     }
 };
