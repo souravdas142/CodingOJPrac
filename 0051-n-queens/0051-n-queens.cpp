9afd7fb07c186424c30dcc179bcef36961d77ced
Sourav Das <souravdas142@gmail.com>
2025-06-11 23:58:13 +0530

    Time: 11 ms (18.48%), Space: 20 MB (12.33%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,58 +1,53 @@
+#define vs vector<string> 
+#define vvs vector<vs >
+
+#define vi vector<int>
+
+
 class Solution {
-private:
-
-    bool checkValidity(int col, int row,int n,vector<bool> &uprightHash,vector<bool>& upleftHash,vector<bool>& rowHash) {
-
-        if(row<n && col<n && rowHash[row]==false && uprightHash[((n-1)+(col-row))]==false && upleftHash[(col+row)]==false) return true;
-
-        
+public:
+    bool checkPossible(int n, int m,int row, int col,vi& rightUpHash, vi& leftUpHash, vi& colHash) {
+        if(row>=0 && col>=0 && row<n && col<m && rightUpHash[row+col]!=1 && leftUpHash[(n-1)+(row-col)]!=1 && colHash[col] !=1 ) return true;
         return false;
     }
-
-    void nqueens(vector<string>& board,int n,int indexcol, vector<vector<string> >& ans,vector<bool> uprightHash,vector<bool> upleftHash,vector<bool> rowHash) {
+    void solve(vs board,int row, vi rightUpHash, vi leftUpHash, vi colHash,vvs& ans, vs res, string s) {
         
-        if(indexcol>=n) {
+        int n = board.size();
+        int m = board[0].size();
+
+        if(row>=n) {
             ans.push_back(board);
             return;
         }
-        
-        /*
 
-            1) there is one queen in each row
-            2) They are not under attack when there is not other queens
-                placed to their diagonal
-            3)there is only one single queens in each column
-
-        */
-
-        for(int row = 0;row<n;row++) {
-
-            if(checkValidity(indexcol,row,n,uprightHash,upleftHash,rowHash)) {
-
-                // we can able to place the queens in the current row
-                // so try for the next row
-                board[row][indexcol] = 'Q';
-                uprightHash[((n-1)+(indexcol-row))] = true;
-                upleftHash[(indexcol+row)] = true;
-                rowHash[row] = true;
-                nqueens(board,n,indexcol+1,ans,uprightHash,upleftHash,rowHash);
-                board[row][indexcol] = '.';
-                uprightHash[((n-1)+(indexcol-row))] = false;
-                upleftHash[(indexcol+row)] = false;
-                rowHash[row] = false;
-                
+        for(int col = 0;col<m;col++) {
+            if(checkPossible(n,m,row,col,rightUpHash,leftUpHash,colHash)) {
+                rightUpHash[row+col] = 1;
+                leftUpHash[(n-1)+(row-col)] = 1;
+                colHash[col] = 1;
+                board[row][col] = 'Q';
+                solve(board,row+1,rightUpHash,leftUpHash,colHash,ans,res,s);
+                board[row][col] = '.';
+                rightUpHash[row+col] = 0;
+                leftUpHash[(n-1)+(row-col)] = 0;
+                colHash[col] = 0;
             }
-
         }
 
+
+
     }
-public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
-        int indexcol = 0;
-        vector<bool> uprightHash((2*n)-1),upleftHash((2*n)-1),rowHash(n);
-        vector<vector<string> > ans;
-        nqueens(board,n,indexcol,ans,uprightHash,upleftHash,rowHash);
+
+        vs board(n,string(n,'.'));
+        vector<int> rightUpHash(n+n,0), leftUpHash(n+n,0), colHash(n,0);
+        vvs ans;
+        vs res;
+        string s = "";
+        int row = 0;
+
+        solve(board,row,rightUpHash,leftUpHash,colHash,ans,res,s);
+
         return ans;
         
     }
