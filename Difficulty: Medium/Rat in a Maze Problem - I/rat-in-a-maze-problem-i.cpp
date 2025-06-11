898c092d5053ce6cba3e858d45ef93681412ac7f
Sourav Das <souravdas142@gmail.com>
2025-06-11 20:43:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,108 +1,80 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 #define vvi vector<vector<int> >
-#define vs vector<string> 
+#define vi vector<int>
+#define vs vector<string>
 #define vvb vector<vector<bool> >
+#define vb vector<bool>
 
-class Cell {
-    public:
-    int row,col;
-    Cell():row(0),col(0) {}
-    Cell(int x,int y):row(x),col(y) {}
-};
-
-vector<int> dx = {1,0,0,-1};
-vector<int> dy = {0,1,-1,0};
+vector<int> dirx = {0,  0,   1,   -1};
+vector<int> diry = {1,  -1,  0,    0};
+vector<char> dir = {'R','L','D','U'};
+                //  up down right left
 
 class Solution {
-    bool checkPossible(int row, Cell cell,vvb& visited,vvi& maze) {
+  public:
+  
+    bool isValidPos(int x,int y, vvi& maze,vvb& visited) {
         
-        if(cell.row>=0 && cell.col>=0 && cell.row<row && cell.col<row && visited[cell.row][cell.col]!=true && maze[cell.row][cell.col]!=0) return true;
+        int row = maze.size();
+        int col = maze[0].size();
+        
+        if(x>=0 && x<row && y>= 0 && y<col && maze[x][y]==1 && visited[x][y] == 1) return true;
         return false;
         
     }
-  public:
   
-    void solve(vvi& maze, vvb& visited,Cell cell,int row, string result,vs& ans) {
+    void solve(vvi& maze, vs& ans, string res, int x, int y,vvb& visited) {
         
-        if(cell.row>=row-1 && cell.col>=row-1) { //n*n matrix row == col
-            ans.push_back(result);
+        int row = maze.size();
+        int col = maze[0].size();
+        if(x==row-1 && y==col-1) {
+            ans.push_back(res);
             return;
         }
         
-        for(int i = 0;i<4;i++) {
-            
-            Cell newCell(cell.row+dx[i],cell.col+dy[i]);
-            
-            if(checkPossible(row,newCell,visited,maze)) {
-                
-                visited[newCell.row][newCell.col] = true;
-                
-                if(i==0) result.push_back('D');
-                else if(i==1) result.push_back('R');
-                else if(i==2) result.push_back('L');
-                else if(i==3) result.push_back('U');
-                solve(maze,visited,newCell,row,result,ans);
-                result.pop_back();
-                visited[newCell.row][newCell.col] = false;
-                
+        
+        
+        for(int i = 0;i<dirx.size();i++) {
+            int nextPosx = x+dirx[i];
+            int nextPosy = y+diry[i];
+            if(isValidPos(nextPosx,nextPosy,maze,visited)) {
+                res.push_back(dir[i]);
+                visited[x][y] = 0;
+                solve(maze,ans,res,nextPosx,nextPosy,visited);
+                res.pop_back();
+                visited[x][y] = 1;
             }
-            
-            
         }
+        
         
     }
   
-    vector<string> findPath(vector<vector<int>> &maze) {
-        // Your code goes here
-        vs ans;
-        int row = maze.size();
-        if(row == 0) return ans;
-        if(maze[0][0]==0) return ans;
-        vvb visited(row,vector<bool>(row,false));
+    vector<string> ratInMaze(vvi& maze) {
+        // code here
         
-        string result;
-        Cell cell;
-        visited[cell.row][cell.col] = true;
-        solve(maze,visited,cell,row,result,ans);
+        if(maze[0][0]==0) return {""};
+        int x = 0, y = 0;
+        int row = maze.size();
+        int col = maze[0].size();
+        
+        
+        vvb visited(row,vb(col,0));
+        
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                visited[i][j] = maze[i][j];
+            }
+        }
+        
+        //vvb visited = maze;
+        
+        vs ans;
+        string res = "";
+        
+        solve(maze,ans,res,x,y,visited);
+        
+        sort(ans.begin(),ans.end());
+        
         return ans;
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> m[i][j];
-            }
-        }
-        Solution obj;
-        vector<string> result = obj.findPath(m);
-        sort(result.begin(), result.end());
-        if (result.size() == 0)
-            cout << -1;
-        else
-            for (int i = 0; i < result.size(); i++)
-                cout << result[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
