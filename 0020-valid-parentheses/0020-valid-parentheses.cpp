24ed4ef0b303d3a2b02a698cfea0438aa9441f05
Sourav Das <souravdas142@gmail.com>
2025-06-14 20:54:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,28 @@
+#define debug(x,y) cout<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<endl;
 class Solution {
 public:
     bool isValid(string s) {
 
+        int n = s.size();
         stack<char> st;
-        int len = s.length();
-        for(int i = 0;i<len;i++) {
-            if(s[i]=='(' || s[i] == '{' || s[i]=='[') {
-                st.push(s[i]);
+        int j = 0;
+        while(j<n) {
+        //debug(s[j],(int)s[j]);
+
+            if(s[j] == '(' || s[j] == '{' || s[j] == '[') {
+                
+                st.push(s[j]);
             }
             else {
-                if(st.size()==0) return false;
-                if(s[i]==')' && st.top()!='(') {
+                if((st.top() == '(' && s[j] == ')') || (st.top() == '{' && s[j] == '}') || (st.top() == '[' && s[j] == ']') ) {
+                    st.pop();
+                }
+                else {
                     return false;
                 }
-                else if(s[i]=='}' && st.top()!='{') {
-                    return false;
-                }
-                else if(s[i]==']' && st.top()!='[') {
-                    return false;
-                }
-                st.pop();
             }
+            j++;
         }
-        return true;
+        return st.empty();
     }
 };
