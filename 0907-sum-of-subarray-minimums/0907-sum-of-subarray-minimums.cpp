60e5ad0081e31e8d34b216b279250d6c03df628d
Sourav Das <souravdas142@gmail.com>
2025-06-18 11:04:10 +0530

    Time: 1808 ms (5.01%), Space: 47.1 MB (78.06%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,100 +1,70 @@
 #define mod ((int)(1e9 +7))
 
-ostream& operator<<(ostream& print,vector<int>& arr) {
-    print<<endl;
-    for(auto a:arr) print<<a<<" ";
-    print<<endl;
+ostream& operator<<(ostream& print, vector<int>& arr) {
+    for(auto e: arr) {
+        print<<e<<" ";
+    }
     return print;
 }
 
 class Solution {
-    void nge(vector<int>& arr,vector<int>& ans,int n) {
-        stack<int> st;
-        int i = n-1;
-        while(i>=0) {
-
-            while(!st.empty() && arr[st.top()]<=arr[i]) {
-                st.pop();
-            }
-
-            if(st.empty()) {
-                ans.push_back(-1);
-            }
-            else {
-                ans.push_back(st.top());
-            }
-
-            st.push(i);
-
-            i--;
-        }
-        reverse(ans.begin(),ans.end());
-    }
-
-    void pse(vector<int>& arr,vector<int>& ans,int n) {
-        stack<int> st;
-        int i = 0;
-        while(i<n) {
-            while(!st.empty() && arr[st.top()]>arr[i]) {
-                st.pop();
-            }
-            if(st.empty()) {
-                ans.push_back(-1);
-            }
-            else {
-                ans.push_back(st.top());
-            }
-            st.push(i);
-
-            i++;
-        }
-    }
-
-    void nse(vector<int>& arr, vector<int>& ans,int n) {
-        stack<int> st;
-        int i  = n-1;
-        while(i>=0) {
-
-            while(!st.empty() && arr[st.top()]>=arr[i]) {
-                st.pop();
-            }
-
-            if(st.empty()) {
-                ans.push_back(n);
-            }
-            else {
-                ans.push_back(st.top());
-            }
-            st.push(i);
-
-            i--;
-        }
-        reverse(ans.begin(),ans.end());
-    }
-
-
-   
 public:
-    int sumSubarrayMins(vector<int>& arr) {
+    void pse(vector<int>& arr, int n, vector<int>& psePos) {
+        stack<int> st;
+        int j = 0;
 
-        int sz = arr.size();
-        
-        vector<int> vnse,vpse;
-        pse(arr,vpse,sz);
-        nse(arr,vnse,sz);
-
-        //cout<<vpse<<endl;
-        //cout<<vnse<<endl;
-
-        int ans = 0;
-        for(int i = 0;i<sz;i++) {
-            int nc = vnse[i]-i;
-            int pc = i-vpse[i];
-            
-            
-            ans = (ans + (pc * nc * 1LL * arr[i]) % mod) % mod;
+        while(j<n) {
+            while(!st.empty() && arr[st.top()]> arr[j]) {
+                st.pop();
+            }
+            if(!st.empty()) {
+                psePos[j] = st.top();
+            }
+            st.push(j);
+            j++;
+        }
+    }
+    void nse(vector<int>& arr, int n, vector<int>& nsePos) {
+        stack<int> st;
+        int j = n-1;
+        while(j>=0) {
+            while(!st.empty() && arr[st.top()]>=arr[j]) {
+                st.pop();
+            }
+            if(!st.empty()) {
+                nsePos[j] = st.top();
+            }
+            st.push(j);
+            j--;
         }
 
-        return (ans%mod);
+    }
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+        vector<int> psePos(n,-1),nsePos(n,n);
+        pse(arr,n,psePos);
+        nse(arr,n,nsePos);
+
+
+        cout<<arr<<endl<<psePos<<endl<<nsePos<<endl;
+
+        // calculate individual Contribution of each
+
+        int j = 0;
+        while(j<n) {
+
+            int leftLenIncludingCur = j - psePos[j];
+            int rightLenIncludingCur = nsePos[j]-j;
+
+            ans = (ans + (leftLenIncludingCur * 1LL * rightLenIncludingCur * arr[j]) % mod) % mod;
+            j++;
+            
+        }
+
+        return ans;
+
+        
     }
 };
