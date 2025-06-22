e57fcd98cb406835255b62ecaee6f69c305ab95c
Sourav Das <souravdas142@gmail.com>
2025-06-22 19:57:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (99.96%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,35 +12,70 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+
         if(p==nullptr && q==nullptr) return true;
-        if(!(p&&q)) return false;
+        if(p==nullptr) return false;
+        if(q==nullptr) return false;
 
         stack<TreeNode*> pst,qst;
+        map<TreeNode*,bool> pmark,qmark;
+        pmark[nullptr]=true;
+        qmark[nullptr]=true;
         pst.push(p);
         qst.push(q);
 
-        
-
         while(!pst.empty() && !qst.empty()) {
 
-            TreeNode* curp = pst.top();
-            TreeNode* curq = qst.top();
-            if(curp->val != curq->val) return false;
+            TreeNode* pcur = pst.top();
+            TreeNode* qcur = qst.top();
+
+            while(pcur->left!=nullptr && pmark[pcur->left]!=true) {
+                pcur = pcur->left;
+                cout<<"pcur val: "<<pcur->val<<endl;
+                pst.push(pcur);
+            }
+            while(qcur->left!=nullptr && qmark[qcur->left]!=true) {
+                qcur = qcur->left;
+                cout<<"qcur val: "<<qcur->val<<endl;
+                qst.push(qcur);
+            }
+
+            //visited
+            pmark[pst.top()]=true;
+            qmark[qst.top()]=true;
+
+            // if their structure is not similar
+            cout<<endl;
+            cout<<"pst.size(): "<<pst.size()<<endl;
+            cout<<"pst.top()->val: "<<pst.top()->val<<endl;
+            cout<<"qst.size(): "<<qst.size()<<endl;
+            cout<<"qst.top()->val: "<<qst.top()->val<<endl;
+            if(pst.size()!=qst.size()) {
+                cout<<endl;
+                cout<<pst.size()<<endl;
+                cout<<qst.size()<<endl;
+                return false;
+
+            }
+
             
+            if(pst.top()->val!=qst.top()->val)
+                return false;
+
+            pcur=pst.top()->right;
+
+            qcur=qst.top()->right;
 
             pst.pop();
             qst.pop();
 
-            
-
-            if(curp->right!=nullptr) pst.push(curp->right);
-            if(curq->right!=nullptr) qst.push(curq->right);
-            if(pst.size()!=qst.size()) return false;
-            if(curp->left!=nullptr) pst.push(curp->left);
-            if(curq->left!=nullptr) qst.push(curq->left);
-            if(pst.size()!=qst.size()) return false;
+            if(pcur!=nullptr) pst.push(pcur);
+            if(qcur!=nullptr) qst.push(qcur);
 
         }
+
+        if(!pst.empty()) return false;
+        if(!qst.empty()) return false;
 
         return true;
         
