862b55b7fce0b9ab35e93402c4cb4af14cda9a47
Sourav Das <souravdas142@gmail.com>
2025-06-30 11:04:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -1,6 +1,22 @@
 class Solution {
   public:
-  
+    bool checkPossible(vector<int>& stations, int maxStations, double dist) {
+        int station = 0; 
+        
+        int n =stations.size();
+        
+        for(int i = 1;i<n;i++) {
+            long double diff = stations[i]-stations[i-1];
+            station = station+ floor(diff/dist);
+            if(station>maxStations) return false;
+            
+        }
+        return true;
+    }
+    
+    /*
+    
+    
     bool is_possible(vector<int>& stations,int n, double maxdiff,int k) {
         int cnt = 0;
        
@@ -15,19 +31,35 @@ class Solution {
         return true;
         
     }
+    
+    */
+    
+    
+    
     double findSmallestMaxDist(vector<int> &stations, int k) {
         // Code here
-        int n = stations.size();
-        long double low = 0;
+        
+        long double low = INT_MAX;
         long double high = INT_MIN;
-        for(int j = 1;j<n;j++) {
-            high = max(high,((long double)stations[j]-stations[j-1]));
-        }
-        long double ans = high;
-        while(high-low>1e-7) {
-            double mid = low+(high-low)/2.0;
+        int n = stations.size();
+        
+        for(int i = 1;i<n;i++) {
             
-            if(is_possible(stations,n,mid,k)) {
+            //low = min((int)low,stations[i]-stations[i-1]);
+            high = max(high,((long double)stations[i]-stations[i-1]));
+        }
+        
+        low = 0;
+        
+        double ans = high;
+        
+        while(high-low>1e-7) {
+            
+            double mid = low+(high-low)/2.0f;
+            
+            //cout<<mid<<endl;
+            
+            if(checkPossible(stations,k,mid)) {
                 ans = mid;
                 high = mid;
             }
@@ -37,5 +69,6 @@ class Solution {
         }
         
         return round(ans*100)/100;
+        
     }
 };
