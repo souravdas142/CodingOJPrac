03b71e126b625145c49f05dde5fd92ba8de18ff7
Sourav Das <souravdas142@gmail.com>
2025-07-01 14:43:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -1,60 +1,27 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-#define ll long long
 class Solution {
   public:
-    // Function to return the minimum cost of connecting the ropes.
-    long long minCost(vector<long long>& arr) {
-        // Your code here
-        priority_queue<ll,vector<ll>, greater<ll>> pq;
+    int minCost(vector<int>& arr) {
+        // code here
         int n = arr.size();
-        for(int i = 0;i<n;i++) {
-            pq.push(arr[i]);
+         priority_queue<int, vector<int> , greater<int>> pq;
+        
+        for(auto e: arr) {
+            pq.push(e);
         }
         
-        ll ans = 0;
+        int ans = 0;
         
         while(pq.size()>1) {
-            ll a = pq.top();
+            int a = pq.top();
+            ans+=a;
             pq.pop();
-            ll b = pq.top();
+            int b = pq.top();
+            ans+=b;
             pq.pop();
-            ll c = a+b;
-            ans+= c;
-            pq.push(c);
+            pq.push(a+b);
+            
         }
         
         return ans;
-        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    long long t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        long long num;
-        vector<long long> a;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-        Solution ob;
-        cout << ob.minCost(a) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
