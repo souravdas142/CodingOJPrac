1c2354d014bd76ce55a49460e033a4708e1cc582
Sourav Das <souravdas142@gmail.com>
2025-07-05 17:10:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -2,26 +2,18 @@ class Solution {
   public:
     int minCost(vector<int>& arr) {
         // code here
-        int n = arr.size();
-         priority_queue<int, vector<int> , greater<int>> pq;
-        
-        for(auto e: arr) {
-            pq.push(e);
-        }
-        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto e: arr) pq.push(e);
         int ans = 0;
-        
         while(pq.size()>1) {
             int a = pq.top();
-            ans+=a;
             pq.pop();
             int b = pq.top();
-            ans+=b;
             pq.pop();
-            pq.push(a+b);
-            
+            int sum = a+b;
+            ans+=sum;
+            pq.push(sum);
         }
-        
         return ans;
     }
 };
