8ed1c84905cc482204ca981f53feec4b6750d564
Sourav Das <souravdas142@gmail.com>
2025-07-07 20:13:27 +0530

    Time: 0 ms (100%), Space: 13.3 MB (42.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,52 +1,38 @@
-#define vvi vector<vector<int> >
 class Solution {
-    int fupb(vvi& matrix,int target) {
-        int row = matrix.size();
-        int low = 0, high = row;
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int low = 0;
+        int high = n-1;
+        int ans = 0;
+        while(low<=high) {
+            int mid = low+(high-low)/2;
+            if(matrix[mid][0]<=target) {
+                ans = mid;
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        //cout<<ans<<"dff"<<high<<"djfjd"<<low<<endl;
+
+        low = 0, high = matrix[ans].size()-1;
+        int ans2 = 0;
+
         while(low<high) {
             int mid = low+(high-low)/2;
-            if(matrix[mid][0]>target) {
+            if(matrix[ans][mid]>=target) {
                 high = mid;
             }
             else {
                 low = mid+1;
             }
         }
-        return low;
-    }
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int upb = fupb(matrix,target);
-        cout<<upb<<endl;
-        int rpos = -1;
-        if(upb==row-1) {
-            if(target<=matrix[row-1][col-1] && target>=matrix[row-1][0]) {
-                rpos = upb;
-            }
-            else if(row-2>=0 && target<=matrix[row-2][col-1] && target>=matrix[row-2][0]) {
-                rpos = upb-1;
-            }
-            else {return false;}
-            
-            
-        }
-        if(target<matrix[0][0]) return false;
-        rpos = upb-1;
-        int low = 0,high = col-1;
-        while(low<=high) {
-            int mid = low+(high-low)/2;
-            if(matrix[rpos][mid]==target) return true;
-            else if(matrix[rpos][mid]>target) {
-                high = mid-1;
-            }
-            else {
-                low = mid+1;
-            }
-        }
 
-        return false;
+        if(matrix[ans][high]!=target) return false;
         
+
+        return true;
     }
 };
