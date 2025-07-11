bb9e6bf4d98794449b3c74e50776e5fbd4233767
Sourav Das <souravdas142@gmail.com>
2025-07-11 23:56:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Balanced Tree Check/balanced-tree-check.cpp
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.cpp
@@ -17,22 +17,24 @@ class Node {
 
 class Solution {
   public:
-    int dfs(Node* root, int &ans) {
+    int dfs(Node* root, int level, int& ans) {
         if(root==nullptr) return 0;
-        int left = dfs(root->left,ans);
-        if(left<0) return -1;
-        int right = dfs(root->right,ans);
-        if(right<0) return -1;
-        if(abs(right-left)>1)return -1;
-        
+        int left = dfs(root->left,level+1,ans);
+        if(left==-1) return -1;
+        int right = dfs(root->right,level+1,ans);
+        if(right==-1) return -1;
+        if(abs(left-right)>1) {
+            ans = -1;
+            return -1;
+        }
         return 1+max(left,right);
     }
     bool isBalanced(Node* root) {
         // Code here
+        int level = 0;
         int ans = 0;
-        int k = 0;
-        k = dfs(root,ans);
-        if(k<0) return false;
+        dfs(root,level,ans);
+        if(ans == -1) return false;
         return true;
     }
 };
