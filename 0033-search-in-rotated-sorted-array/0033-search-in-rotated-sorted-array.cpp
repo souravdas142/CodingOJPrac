a404f3ec10013357575cd4445c462d6856c0d1b0
Sourav Das <souravdas142@gmail.com>
2025-07-11 23:48:02 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,29 +2,37 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int sz = nums.size();
-        int left = 0, right = sz-1;
-        while(left<right) {
-
-            int mid = left+(right-left)/2;
-
-            if(nums[mid]>=nums[left]) { //sorted
-
-                if(target>=nums[left] && target<=nums[mid])
-                    right = mid;
-                else 
-                    left = mid+1;
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while(low<high) {
+            int mid = low+(high-low)/2;
+            
+            if(nums[mid]>=nums[low]) { // sorted
+                if(nums[low]<=target && nums[mid]>=target) {
+                    high = mid;
+                }
+                else {
+                    low = mid+1;
+                }
             }
-            else {  
-                if(target>=nums[mid+1] && target<=nums[right])
-                    left = mid+1;
-                else 
-                    right = mid;
+            else {
+                if(nums[mid]<=target && nums[high]>=target) {
+                    low = mid;
+                }
+                else {
+                    high = mid;
+                }
             }
+            
+
+
         }
-        cout<<left<<" "<<right<<endl;
-        if(nums[left]!=target) return -1;
-        return left;
+
+        if(nums[high]!=target) return -1;
+        return high;
+
+    
         
     }
 };
