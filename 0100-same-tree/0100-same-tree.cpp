d73e4253a6f910aee34d052e3e2628e0a85bf6d0
Sourav Das <souravdas142@gmail.com>
2025-07-12 02:15:34 +0530

    Time: 0 ms (100%), Space: 12.8 MB (47.92%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,72 +12,15 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-
-        if(p==nullptr && q==nullptr) return true;
-        if(p==nullptr) return false;
-        if(q==nullptr) return false;
-
-        stack<TreeNode*> pst,qst;
-        map<TreeNode*,bool> pmark,qmark;
-        pmark[nullptr]=true;
-        qmark[nullptr]=true;
-        pst.push(p);
-        qst.push(q);
-
-        while(!pst.empty() && !qst.empty()) {
-
-            TreeNode* pcur = pst.top();
-            TreeNode* qcur = qst.top();
-
-            while(pcur->left!=nullptr && pmark[pcur->left]!=true) {
-                pcur = pcur->left;
-                cout<<"pcur val: "<<pcur->val<<endl;
-                pst.push(pcur);
-            }
-            while(qcur->left!=nullptr && qmark[qcur->left]!=true) {
-                qcur = qcur->left;
-                cout<<"qcur val: "<<qcur->val<<endl;
-                qst.push(qcur);
-            }
-
-            //visited
-            pmark[pst.top()]=true;
-            qmark[qst.top()]=true;
-
-            // if their structure is not similar
-            cout<<endl;
-            cout<<"pst.size(): "<<pst.size()<<endl;
-            cout<<"pst.top()->val: "<<pst.top()->val<<endl;
-            cout<<"qst.size(): "<<qst.size()<<endl;
-            cout<<"qst.top()->val: "<<qst.top()->val<<endl;
-            if(pst.size()!=qst.size()) {
-                cout<<endl;
-                cout<<pst.size()<<endl;
-                cout<<qst.size()<<endl;
-                return false;
-
-            }
-
-            
-            if(pst.top()->val!=qst.top()->val)
-                return false;
-
-            pcur=pst.top()->right;
-
-            qcur=qst.top()->right;
-
-            pst.pop();
-            qst.pop();
-
-            if(pcur!=nullptr) pst.push(pcur);
-            if(qcur!=nullptr) qst.push(qcur);
-
+        if(p==nullptr || q==nullptr) {
+            if(p==nullptr && q==nullptr) return true;
+            return false;
         }
-
-        if(!pst.empty()) return false;
-        if(!qst.empty()) return false;
-
+        if(p!=nullptr && q!=nullptr) {
+            if(p->val!=q->val) return false;
+        }
+        if(isSameTree(p->left,q->left)==false) return false;
+        if(isSameTree(p->right,q->right)==false) return false;
         return true;
-        
     }
 };
