35e6b4e918d436f644394fd91d41e20154d4d859
Sourav Das <souravdas142@gmail.com>
2025-07-12 21:30:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transform to Sum Tree/transform-to-sum-tree.cpp
+++ Difficulty: Easy/Transform to Sum Tree/transform-to-sum-tree.cpp
@@ -10,18 +10,24 @@ class Solution {
 
     // Convert a given tree to a tree where every node contains sum of values of
     // nodes in left and right subtrees in the original tree
-    int dfs(Node* root) {
-        if(root==nullptr) return 0;
-        int left = dfs(root->left);
-        int right = dfs(root->right);
-        int tmp = root->data;
-        int res = left+right;
-        root->data = res;
-        //if(root->left == nullptr && root->right==nullptr) root->data = 0;
-        return res+tmp;
+    int dfs(Node* node) {
+        if(node==nullptr) return 0;
+        if(node->left==nullptr && node->right == nullptr) {
+            
+            int x = node->data;
+            node->data = 0;
+            return x;
+        }
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        int y = node->data;
+        node->data = left+right;
+        return node->data+y;
     }
     void toSumTree(Node *node) {
         // Your code here
+        
         dfs(node);
+        
     }
 };
