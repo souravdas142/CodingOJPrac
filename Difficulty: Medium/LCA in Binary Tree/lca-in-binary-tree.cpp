e2ef1afb59d6c5caa9697b1bbccb01a2e1a7eba9
Sourav Das <souravdas142@gmail.com>
2025-07-12 21:55:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
@@ -18,14 +18,16 @@ class Solution {
     // Function to return the lowest common ancestor in a Binary Tree.
     Node* lca(Node* root, int n1, int n2) {
         // Your code here
-        if(!root) return nullptr;
+        if(root==nullptr) return root;
         if(root->data == n1 || root->data == n2) {
             return root;
         }
-        Node* left =  lca(root->left,n1,n2);
-        Node* right =  lca(root->right,n1,n2);
+        Node* left = lca(root->left,n1,n2);
+        Node* right = lca(root->right,n1,n2);
+        
         if(left!=nullptr && right!=nullptr) return root;
-        if(!left) return right;
-        return left;
+        if(left==nullptr) return right;
+        if(right==nullptr) return left;
+        
     }
 };
