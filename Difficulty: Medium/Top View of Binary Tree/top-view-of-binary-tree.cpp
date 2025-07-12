8952f4e4b4cbe38c7ffd634f81c3b66ddf634ded
Sourav Das <souravdas142@gmail.com>
2025-07-12 11:35:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -6,44 +6,36 @@ struct Node
     Node* right;
 };
 */
-
 class Solution {
   public:
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
-    vector<int> topView(Node *root) {
-        // code here
-        vector<int> ans;
-        if(!root) return ans;
-        
-        map<int,int> mans;
-        queue<pair<int,pair<int,Node*> > > que;
-        
-        
-        que.push({0,{0,root}});
-        while(!que.empty()) {
-            
-            int n = que.size();
-            for(int i = 0;i<n;i++) {
-                pair<int,pair<int,Node*> > curNodes = que.front();
-                if(mans.find(curNodes.first)==mans.end()) {
-                    mans[curNodes.first] = curNodes.second.second->data;
-                }
-                que.pop();
-                
-                if(curNodes.second.second->left) {
-                    que.push({curNodes.first-1,{curNodes.second.first+1,curNodes.second.second->left}});
-                    
-                }
-                if(curNodes.second.second->right) {
-                    que.push({curNodes.first+1,{curNodes.second.first+1,curNodes.second.second->right}});
-                }
+    void dfs(Node* root,int cord,int level, map<int,pair<int,int>>& mp) {
+        if(root==nullptr) return;
+        if(mp.find(cord)==mp.end()) {
+            mp.insert({cord,{level,root->data}});
+        }
+        else {
+            if(mp[cord].first>level) {
+                mp[cord].first = level;
+                mp[cord].second = root->data;
             }
         }
+        dfs(root->left,cord-1,level+1,mp);
+        dfs(root->right,cord+1,level+1,mp);
+    }
+    vector<int> topView(Node *root) {
         
-        for(auto ele: mans) {
-            ans.push_back(ele.second);
+        // code here
+        int co_ordinates = 0;
+        int level = 0;
+        map<int,pair<int,int> > mp;
+        dfs(root,co_ordinates,level,mp);
+        vector<int> ans;
+        for(auto it:mp) {
+            ans.push_back(it.second.second);
         }
         return ans;
+        
     }
 };
