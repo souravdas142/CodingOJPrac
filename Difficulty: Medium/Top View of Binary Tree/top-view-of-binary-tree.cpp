6eee6d352f97ad508dbca33cba3bda8109f776d6
Sourav Das <souravdas142@gmail.com>
2025-07-12 17:58:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -6,36 +6,58 @@ struct Node
     Node* right;
 };
 */
+
+#define vpii vector<pair<int,int>>
 class Solution {
   public:
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
-    void dfs(Node* root,int cord,int level, map<int,pair<int,int>>& mp) {
+    
+    void dfs(Node* root,vpii& pos,vpii& neg, int level,int cord) {
         if(root==nullptr) return;
-        if(mp.find(cord)==mp.end()) {
-            mp.insert({cord,{level,root->data}});
-        }
-        else {
-            if(mp[cord].first>level) {
-                mp[cord].first = level;
-                mp[cord].second = root->data;
+        if(cord>=0) {
+            if(pos.size()>cord && pos[cord].first>level) {
+                pos[cord].first = level;
+                pos[cord].second = root->data;
+            }
+            else if(pos.size()<=cord) {
+                pos.push_back({level,root->data});
             }
         }
-        dfs(root->left,cord-1,level+1,mp);
-        dfs(root->right,cord+1,level+1,mp);
+        else {
+            int negCord = (0-cord);
+            if(neg.size()>negCord && neg[negCord].first>level) {
+                neg[negCord].first = level;
+                neg[negCord].second = root->data;
+            }
+            else if(neg.size()<=negCord) {
+                neg.push_back({level,root->data});
+            }
+        }
+        dfs(root->left,pos,neg,level+1,cord-1);
+        dfs(root->right,pos,neg,level+1,cord+1);
     }
+    
     vector<int> topView(Node *root) {
-        
         // code here
-        int co_ordinates = 0;
-        int level = 0;
-        map<int,pair<int,int> > mp;
-        dfs(root,co_ordinates,level,mp);
+        vector<pair<int,int>> pos,neg;
+        neg.push_back({-1,-1});
+        
         vector<int> ans;
-        for(auto it:mp) {
-            ans.push_back(it.second.second);
+        int level = 0;
+        int cord = 0;
+        
+        dfs(root,pos,neg,level,cord);
+        
+        for(int i = 1;i<neg.size();i++) {
+            ans.push_back(neg[i].second);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        
+        for(int i = 0;i<pos.size();i++) {
+            ans.push_back(pos[i].second);
         }
         return ans;
-        
     }
 };
