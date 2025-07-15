d1f0fede50cd83e0c706aa35587b8045d9fde14d
Sourav Das <souravdas142@gmail.com>
2025-07-15 16:02:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -1,83 +1,54 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
+#define graph_t vector<vector<int>> 
+#define visit_t vector<vector<bool>>
 
-// } Driver Code Ends
-
-#define vvi vector<vector<int> >
 class Solution {
-private:
-    bool check(vvi& image,vvi& visited,int sr,int sc,int newx,int newy,int n,int m) {
-        if(newx>=0 && newy >= 0 &&  newx<n && newy<m && image[sr][sc]==image[newx][newy] && visited[newx][newy]==false) return true;
+  public:
+  
+    vector<int> dirx = {0, 0, 1, -1};
+    vector<int> diry = {1, -1, 0, 0};
+    
+    bool checkPos(graph_t& graph, visit_t& visited, int sr, int sc) {
+        int n = graph.size();
+        int m = graph[0].size();
+        if(sr>=0 && sr<n && sc>=0 && sc<m && visited[sr][sc]==false) return true;
         return false;
     }
-    void bfs(vvi& image,vvi& visited,int sr,int sc,int newColor) {
-        
-        int n = image.size();
-        int m = image[0].size();
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
-        visited[sr][sc] = true;
-        deque<pair<int,int> > dq;
-        dq.push_back({sr,sc});
-        
-        while(!dq.empty()) {
-            
-            pair<int,int> ele = dq.front();
-            dq.pop_front();
-            for(int i = 0;i<4;i++) {
-                int newx = ele.first+dx[i];
-                int newy = ele.second+dy[i];
-                if(check(image,visited,sr,sc,newx,newy,n,m)) {
-                    dq.push_back({newx,newy});
-                    image[newx][newy] = newColor;
-                    visited[newx][newy] = true;
-                }
-            }
-            
-        }
-    }
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        // Code here 
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int> > visited(n,vector<int>(m,false));
-        
-        
-        bfs(image,visited,sr,sc,newColor);
+  
+    void dfs(graph_t& image,visit_t& visited, int sr, int sc, int color,int newColor) {
         
         image[sr][sc] = newColor;
+        visited[sr][sc] = true;
         
-             
+        for(int i = 0;i<4;i++) {
+            if(checkPos(image,visited,sr+dirx[i],sc+diry[i])) {
+                dfs(image,visited,sr+dirx[i],sc+diry[i],color,newColor);
+            }
+        }
         
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int newColor) {
+        // Code here
+        int n = image.size();
+        int m = image[0].size();
+    
+        visit_t visited(n,vector<bool>(m,false));
+        
+        int color = image[sr][sc];
+        
+        for(int i =0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(image[i][j]!=color) {
+                    visited[i][j] = true;
+                }
+            }
+        }
+        
+        dfs(image,visited,sr,sc,color,newColor);
         
         return image;
+    
+        
+        
     }
 };
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>image(n, vector<int>(m,0));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++)
-				cin >> image[i][j];
-		}
-		int sr, sc, newColor;
-		cin >> sr >> sc >> newColor;
-		Solution obj;
-		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
-		for(auto i: ans){
-			for(auto j: i)
-				cout << j << " ";
-			cout << "\n";
-		}
-	}
-	return 0;
-}
-// } Driver Code Ends
