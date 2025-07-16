2854ba45766b2a7735c35abb54900da6d87e02cd
Sourav Das <souravdas142@gmail.com>
2025-07-17 01:07:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,97 +1,74 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
+#define graph_t vector<vector<int>>
+#define visit_t vector<vector<bool>>
 
-// } Driver Code Ends
-
-#define vvi vector<vector<int> >
-#define vvb vector<vector<bool> >
-#define vb vector<bool>
-#define vi vector<int>
-
-class Node {
-public:
-    int row;
-    int col;
-    int dist;
-    
-    Node(int r,int c,int d):row(r),col(c),dist(d) {};
-    
-    
+class cell {
+    public:
+        int i;
+        int j;
+        int dist;
+        cell() {
+            i = 0;
+            j = 0;
+            dist = 0;
+        }
+        cell(int a, int b, int c=0):i(a),j(b),dist(c) {}
 };
-
-vector<int> dx = {0,0,1,-1};
-vector<int> dy = {1,-1,0,0};
 
 class Solution {
   public:
+    vector<int> dirx = {0,0,1,-1};
+    vector<int> diry = {1,-1,0,0};
+    
+    bool valid(int i, int j,int n, int m, graph_t& grid,visit_t& visited) {
+        if(i>=0 && i<n && j>=0 && j<m && visited[i][j]==false)
+            return true;
+        return false;
+    }
+
+  
     // Function to find distance of nearest 1 in the grid for each cell.
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
         // Code here
         int n = grid.size();
         int m = grid[0].size();
+        visit_t visited(n,vector<bool>(m,false));
+        graph_t gridAns(n,vector<int>(m,0));
         
-        vvi ans(n,vi(m,0));
-        vvb visited(n,vb(m,0));
+     
         
-        deque<Node> dq;
+        queue<cell> que;
         
         for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                if(grid[i][j] == 1) {
-                    dq.push_back(Node(i,j,0));
+            for(int j  = 0;j<m;j++) {
+                if(grid[i][j]==1) {
+                    que.push(cell(i,j));
                     visited[i][j] = true;
-                }   
+                }
             }
         }
         
-        
-        while(!dq.empty()) {
-            
-            Node nd = dq.front();
-            dq.pop_front();
-            
-            ans[nd.row][nd.col] = nd.dist;
+        while(!que.empty()) {
+            cell cl = que.front();
+            que.pop();
             
             for(int i = 0;i<4;i++) {
-                Node newNode(nd.row+dx[i],nd.col+dy[i],nd.dist+1);
-                if(newNode.row>=0 && newNode.col>=0 && newNode.row<n && newNode.col<m && !visited[newNode.row][newNode.col]) {
-                    ans[newNode.row][newNode.col] = newNode.dist;
-                    visited[newNode.row][newNode.col] = true;
-                    dq.push_back(newNode);
+                
+                cell newCell(cl.i+dirx[i],cl.j+diry[i]);
+                
+                if(valid(newCell.i,newCell.j,n,m,grid,visited)) {
+                   
+                    newCell.dist = cl.dist+1;
+                    gridAns[newCell.i][newCell.j] = cl.dist+1;
+                    
+                    visited[newCell.i][newCell.j] = true;
+                    que.push(newCell);
                 }
             }
             
+            
         }
-        
-        return ans;
-        
+       
+        return gridAns;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> grid(n, vector<int>(m, -1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> grid[i][j];
-            }
-        }
-        Solution obj;
-        vector<vector<int>> ans = obj.nearest(grid);
-        for (auto i : ans) {
-            for (auto j : i) {
-                cout << j << " ";
-            }
-            cout << "\n";
-        }
-    }
-    return 0;
-}
-// } Driver Code Ends
