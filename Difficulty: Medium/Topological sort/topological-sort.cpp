322148d1832e63dab299502be204b7e3ffa1b84e
Sourav Das <souravdas142@gmail.com>
2025-07-20 21:33:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,52 +1,44 @@
 
-#define graph_t vector<vector<int>>
+#define graph_t vector<vector<int>> 
 #define visit_t vector<bool>
 
-ostream& operator<<(ostream& print,vector<int>& tmp) {
-    for(auto& e: tmp) {
-        print<<e<<" ";
-    }
-    return print;
-}
-
 class Solution {
-  public:
-    bool dfs(graph_t& adj, visit_t& visited,visit_t& dfsvisited,int u, vector<int>& ans) {
+    bool dfs(visit_t& visited,visit_t& dfsVisited, graph_t& adj, vector<int>& ans, int u) {
         visited[u] = true;
-        dfsvisited[u]=true;
-        
-        for(auto& v:adj[u]) {
+        dfsVisited[u] = true;
+        for(auto& v: adj[u]) {
             if(!visited[v]) {
-                dfs(adj,visited,dfsvisited, v, ans);
+                
+                if(dfs(visited,dfsVisited,adj,ans,v)==true) return true;
             }
-            // else if(dfsvisited[v]){
-            //     return true;
-            // }
+            else if(dfsVisited[v]==true) {
+                return true;
+            }
         }
         ans.push_back(u);
-        dfsvisited[u] = false;
+        dfsVisited[u] = false;
         return false;
-        
     }
-    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+  public:
+    vector<int> topoSort(int V, graph_t& edges) {
         // code here
         graph_t adj(V);
-        visit_t visited(V,0);
+        visit_t visited(V,false);
         int n = edges.size();
-        
-        for(int i = 0;i<n;i++) {
-            adj[edges[i][0]].push_back(edges[i][1]);
+        for(auto& pr: edges) {
+            adj[pr[0]].push_back(pr[1]);
+          
         }
+        
         vector<int> ans;
+        vector<bool> dfsVisited(V,false);
+        
         for(int i = 0;i<V;i++) {
             if(!visited[i]) {
-                visit_t dfsvisited(V,0);
-                dfs(adj,visited,dfsvisited,i,ans);
-                
+                if(dfs(visited,dfsVisited,adj,ans,i)==true) return {};
             }
         }
         reverse(ans.begin(),ans.end());
-        //cout<<ans<<endl;
         return ans;
     }
 };
