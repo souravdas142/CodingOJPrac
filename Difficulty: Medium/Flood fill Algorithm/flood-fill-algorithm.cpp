b1d8dd492e3a51145bdf954739a7f928890f2b5c
Sourav Das <souravdas142@gmail.com>
2025-07-29 16:34:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -1,54 +1,35 @@
-#define graph_t vector<vector<int>> 
+#define grid_t vector<vector<int>>
 #define visit_t vector<vector<bool>>
 
 class Solution {
   public:
-  
-    vector<int> dirx = {0, 0, 1, -1};
-    vector<int> diry = {1, -1, 0, 0};
-    
-    bool checkPos(graph_t& graph, visit_t& visited, int sr, int sc) {
-        int n = graph.size();
-        int m = graph[0].size();
-        if(sr>=0 && sr<n && sc>=0 && sc<m && visited[sr][sc]==false) return true;
-        return false;
-    }
-  
-    void dfs(graph_t& image,visit_t& visited, int sr, int sc, int color,int newColor) {
-        
-        image[sr][sc] = newColor;
-        visited[sr][sc] = true;
-        
-        for(int i = 0;i<4;i++) {
-            if(checkPos(image,visited,sr+dirx[i],sc+diry[i])) {
-                dfs(image,visited,sr+dirx[i],sc+diry[i],color,newColor);
-            }
-        }
-        
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int newColor) {
-        // Code here
+    vector<int> dirx = {0,0,1,-1};
+    vector<int> diry = {1,-1,0,0};
+    bool checkValid(grid_t& image, int sr, int sc, int color,visit_t& visited) {
         int n = image.size();
         int m = image[0].size();
-    
-        visit_t visited(n,vector<bool>(m,false));
-        
-        int color = image[sr][sc];
-        
-        for(int i =0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                if(image[i][j]!=color) {
-                    visited[i][j] = true;
-                }
+        if(sr>=0 && sr<n && sc>=0 && sc<m && visited[sr][sc]!=true && image[sr][sc]==color) return true;
+        return false;
+    }
+    void dfs(grid_t& image,int sr, int sc,visit_t& visited,int color, int newColor) {
+        image[sr][sc] = newColor;
+        visited[sr][sc] = true;
+        for(int i  = 0;i<4;i++) {
+            int nsr = sr+dirx[i];
+            int nsc = sc+diry[i];
+            if(checkValid(image,nsr,nsc,color,visited)) {
+                dfs(image,nsr,nsc,visited,color,newColor);
             }
         }
-        
-        dfs(image,visited,sr,sc,color,newColor);
-        
+    }
+    vector<vector<int>> floodFill(grid_t& image, int sr, int sc, int newColor) {
+        // Code here
+        int color = image[sr][sc];
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        dfs(image,sr,sc,visited,color,newColor);
         return image;
-    
-        
         
     }
 };
