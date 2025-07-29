989c1a9f60af4b3eff47efb5af57437ef2383dfd
Sourav Das <souravdas142@gmail.com>
2025-07-29 21:14:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,44 +1,37 @@
-
 #define graph_t vector<vector<int>> 
-#define visit_t vector<bool>
+
 
 class Solution {
-    bool dfs(visit_t& visited,visit_t& dfsVisited, graph_t& adj, vector<int>& ans, int u) {
+  public:
+  
+    void dfs(graph_t& adj, vector<bool>& visited, int u, vector<int>& ans) {
         visited[u] = true;
-        dfsVisited[u] = true;
         for(auto& v: adj[u]) {
             if(!visited[v]) {
-                
-                if(dfs(visited,dfsVisited,adj,ans,v)==true) return true;
-            }
-            else if(dfsVisited[v]==true) {
-                return true;
+                dfs(adj,visited,v,ans);
             }
         }
         ans.push_back(u);
-        dfsVisited[u] = false;
-        return false;
     }
-  public:
-    vector<int> topoSort(int V, graph_t& edges) {
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
+        
         graph_t adj(V);
-        visit_t visited(V,false);
-        int n = edges.size();
-        for(auto& pr: edges) {
-            adj[pr[0]].push_back(pr[1]);
-          
+        for(auto& vec: edges) {
+            adj[vec[0]].push_back(vec[1]);
+        
         }
         
+        vector<bool> visited(V,0);
         vector<int> ans;
-        vector<bool> dfsVisited(V,false);
         
         for(int i = 0;i<V;i++) {
-            if(!visited[i]) {
-                if(dfs(visited,dfsVisited,adj,ans,i)==true) return {};
-            }
+            if(!visited[i])
+                dfs(adj,visited,i,ans);
         }
+        
         reverse(ans.begin(),ans.end());
         return ans;
+        
     }
 };
