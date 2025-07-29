d2b877343fb8f29ccb1f481dbbb7ce4bdca4bc17
Sourav Das <souravdas142@gmail.com>
2025-07-29 22:26:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,35 +1,37 @@
 #define graph_t vector<vector<int>>
-
+#define visit_t vector<bool>
+#define vi vector<int>
 class Solution {
   public:
-    bool dfs(graph_t& adj, int u, vector<int>& visited) {
-        visited[u] = 1;
-        for(auto& v: adj[u]) {
-            if(!visited[v]) {
-                if(dfs(adj,v,visited)==false) return false;;
-            }
-            else if(visited[v]==1) {
-                return false;
-            }
-        }
-        visited[u] = 2;
-        return true;
-    }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         graph_t adj(V);
+        vi indeg(V,0),ans;
         for(auto& vec: edges) {
             adj[vec[0]].push_back(vec[1]);
+            indeg[vec[1]]++;
         }
+        queue<int> que;
         
-        vector<int> visited(V,false);
         for(int i  = 0;i<V;i++) {
-            if(!visited[i]) {
-                if(dfs(adj,i,visited)==false) return true;
+            if(indeg[i]==0) {
+                que.push(i);
             }
-            
         }
-        return false;
         
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            ans.push_back(u);
+            for(auto& v: adj[u]) {
+                indeg[v]--;
+                if(indeg[v]==0) {
+                    que.push(v);
+                }
+            }
+        }
+        // cout<<ans.size()<<endl;
+        if(ans.size()!=V) return true;
+        return false;
     }
 };
