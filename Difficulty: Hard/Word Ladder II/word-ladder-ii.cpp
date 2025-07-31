aa85824b322dc54278c30d4d9fb3a2888cdfaf57
Sourav Das <souravdas142@gmail.com>
2025-07-31 18:11:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Word Ladder II/word-ladder-ii.cpp
+++ Difficulty: Hard/Word Ladder II/word-ladder-ii.cpp
@@ -1,136 +1,68 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 class Solution {
-public:
-    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+  public:
+  
+    void dfs(unordered_map<string,set<string>>& parent, string targetWord, string startWord,vector<string> res, vector<vector<string>>& ans) {
+        res.push_back(startWord);
+        if(startWord == targetWord) {
+            reverse(res.begin(),res.end());
+            ans.push_back(res);
+            return;
+        }
+            
+        for(auto& s: parent[startWord]) {
+            
+            dfs(parent,targetWord,s,res,ans);
+        }
+    }
+  
+    vector<vector<string>> findSequences(string startWord, string targetWord,
+                                         vector<string>& wordList) {
         // code here
-        queue<pair<vector<string>,int> > que;
-        set<string> sets(wordList.begin(),wordList.end());
-       
-        int n = beginWord.length();
-        int m = wordList.size();
-        vector<vector<string> > ans;
-        bool trigger = true;
-
-        que.push({vector<string>({beginWord}),1});
-
-        sets.erase(beginWord);
+                // Code here
+        unordered_map<string,bool> dict;
+        unordered_map<string,int> dist;
+        vector<vector<string>> ans;
         
-        while(!que.empty() && trigger) {
-
-            int sz = que.size();
-
-            queue<string> qst;
-
-            for(int samelevel = 0;samelevel<sz;samelevel++) {
-
-                pair<vector<string>,int> node = que.front();
-                vector<string> list = node.first;
-                            //cout<<endl;
-                            //for(auto a:list) cout<<a<<" -> ";
-                            //cout<<endl;
-                
-                que.pop();
-
-                int dist = node.second;
-
-
-                string word = list.back();
-
-
-
-                for(int i= 0;i<n;i++) {
-                    string modword = word;
-                    for(char ch='a';ch<='z';ch++) {
-                        if(ch==word[i]) continue;
-                        modword[i] = ch;
-                        if(sets.count(modword)) {
-                            list.push_back(modword);
-                            que.push({list,dist+1});
-                            qst.push(modword);
-                            list.pop_back();
-                            if(modword==endWord)
-                                trigger = false;
+        unordered_map<string,set<string>> parent;
+        
+        for(auto& s: wordList) {
+            dict.insert({s,true});
+            dist.insert({s,INT_MAX});
+        }
+        if(dict.find(targetWord)==dict.end()) return ans;
+        
+        dist.insert({startWord,1});
+        dist[startWord] = 1;
+        
+        queue<string> que;
+        que.push(startWord);
+        
+        while(!que.empty()) {
+            string u = que.front();
+            
+            que.pop();
+            
+            int len = u.length();
+            for(int i = 0;i<len;i++) {
+                string temp = u;
+                for(char ch = 'a';ch<='z';ch++) {
+                    if(ch==u[i]) continue;
+                    temp[i] = ch;
+                    if(dict.find(temp)!=dict.end()) {
+                        if(dist[u]+1<=dist[temp]) {
+                            dist[temp] = dist[u]+1;
+                            que.push(temp);
+                            parent[temp].insert(u);
                         }
                     }
                 }
             }
-
-            while(!qst.empty() && trigger) {
-                sets.erase(qst.front());
-                qst.pop();
-            }
-
-
-
-
-
-
-
-        }
-
-        
-       // cout<<que.size()<<endl;
-        while(!que.empty()) {
-            pair<vector<string>,int> node = que.front();
-            que.pop();
-            if(node.first.back()==endWord) {
-                ans.push_back(node.first);
-            }
             
         }
-
+        vector<string> res;
+        dfs(parent,startWord,targetWord,res,ans);
         return ans;
-        
     }
 };
-
-//{ Driver Code Starts.
-
-bool comp(vector<string> a, vector<string> b)
-{
-    string x = "", y = "";
-    for(string i: a)
-        x += i;
-    for(string i: b)
-        y += i;
-    
-    return x<y;
-}
-
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<string>wordList(n);
-		for(int i = 0; i < n; i++)cin >> wordList[i];
-		string startWord, targetWord;
-		cin >> startWord >> targetWord;
-		Solution obj;
-		vector<vector<string>> ans = obj.findSequences(startWord, targetWord, wordList);
-		if(ans.size()==0)
-		    cout<<-1<<endl;
-		else
-		{
-		    sort(ans.begin(), ans.end(), comp);
-            for(int i=0; i<ans.size(); i++)
-            {
-                for(int j=0; j<ans[i].size(); j++)
-                {
-                    cout<<ans[i][j]<<" ";
-                }
-                cout<<endl;
-            }
-		}
-	}
-	return 0;
-}
-// } Driver Code Ends
