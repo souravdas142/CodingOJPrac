dcca11619a83b0a8c031512ddd25942d5de6f2a9
Sourav Das <souravdas142@gmail.com>
2025-08-07 17:12:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (18.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int  countWays(int n,vector<int>& dp) {
+    int dpSol(int n, vector<int>& dp) {
         if(n<=1) return 1;
-        if(dp[n]!=0) return dp[n];
-        dp[n] = countWays(n-1,dp)+countWays(n-2,dp);
+
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = dpSol(n-1,dp)+dpSol(n-2,dp);
+    }
+
+
+    int tabSol(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
+
+
     int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        int ans = countWays(n,dp);
-        return ans;
+        vector<int> dp(n+1,-1);
+        return dpSol(n,dp);
+
+        //return tabSol(n);
+        
     }
 };
