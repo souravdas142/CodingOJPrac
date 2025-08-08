26963bd2e06488f51e4fb1d33f34f266487a2ec2
Sourav Das <souravdas142@gmail.com>
2025-08-08 12:35:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,21 +1,32 @@
 class Solution {
   public:
+    int recurSol(vector<int>& height, int n,vector<int>& dp) {
+        
+        //dp[1] = dp[0] + abs(height[1]-height[0]);
+        
+        if(n==0) return 0;
+        if(n==1) return abs(height[1]-height[0]);
+        
+        if(dp[n]!=-1) return dp[n];
+        
+         return dp[n] = min(
+                
+                recurSol(height,n-1,dp)+abs(height[n-1]-height[n]),
+                recurSol(height,n-2,dp)+abs(height[n-2]-height[n])
+                
+            
+        );
+        
+       
+    }
+    
     int minCost(vector<int>& height) {
         // Code here
+        
+        
         int n = height.size();
-        vector<int> dp(n+1,0);
-        dp[1] = dp[0] + abs(height[1]-height[0]);
-        
-        for(int i  = 2;i<n;i++) {
-            dp[i] = min(
-                
-                dp[i-1]+abs(height[i-1]-height[i]),
-                dp[i-2]+abs(height[i-2]-height[i])
-                
-                );
-        }
-        
-        return dp[n-1];
+        vector<int> dp(n+1,-1);
+        return recurSol(height,n-1,dp);
         
     }
 };
