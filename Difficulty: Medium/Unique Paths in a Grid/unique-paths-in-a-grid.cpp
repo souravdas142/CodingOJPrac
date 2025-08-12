3a51648b28867f521df249b99c2a9479080f1cd0
Sourav Das <souravdas142@gmail.com>
2025-08-12 09:38:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
@@ -32,8 +32,62 @@ class Solution {
         vvi dp(n,vi(m,-1));
         return recurSolve(grid,0,0,dp);
     }
+    int tabulation(vvi& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vvi dp(n,vi(m,0));
+        if(grid[0][0]==1) return 0;
+        dp[0][0] = 1;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(i == 0 && j==0) continue;
+                if(grid[i][j]==1) continue;
+                int left = (i-1>=0 && grid[i-1][j]!=1)?dp[i-1][j]:0;
+                int right = (j-1>=0 && grid[i][j-1]!=1)?dp[i][j-1]:0;
+                dp[i][j] = left+right;
+            }
+        }
+        return dp[n-1][m-1];
+    }
+    int spaceOpt(vvi& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if(grid[0][0]==1) return 0;
+        vi dp(m,1);
+        int way = 1;
+        for(int i = 0;i<m;i++) {
+            if(grid[0][i]==1) way = 0;
+            dp[i] = way;
+        }
+        
+        for(int i =1;i<n;i++) {
+            int prev = 0;
+            vi temp(m,0);
+            for(int j = 0;j<m;j++) {
+                if(grid[i][j]==1) {
+                    int cur = 0;
+                    temp[j] = cur;
+                    prev = cur;
+                    continue;
+                }
+                int cur = 0;
+                if(dp[j]==0 && j>0 && prev==0) cur = 0;
+                else if(dp[j]==0 && !(j>0 && prev==0)) cur = prev;
+                else if(dp[j]!=0 && (j>0 && prev==0)) cur = dp[j];
+                else cur = prev+dp[j];
+                prev = cur;
+                temp[j] = cur;
+            }
+            dp = temp;
+        }
+        return dp[m-1];
+        
+    }
     int uniquePaths(vector<vector<int>> &grid) {
         // code here
-        return recurSol(grid);
+        //return recurSol(grid);
+        //return tabulation(grid);
+        return spaceOpt(grid);
+        
     }
 };
