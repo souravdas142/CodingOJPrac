5575c6e0020cf3192fb6369c449f93582e983f95
Sourav Das <souravdas142@gmail.com>
2025-08-13 10:49:08 +0530

    Time: 3 ms (15.09%), Space: 13.5 MB (5.56%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -27,9 +27,45 @@ public:
         
         return recurSolve(triangle,n,0,0,dp);
     }
+
+
+    int tabulation(vvi& triangle) {
+        int n = triangle.size();
+        vvi dp(triangle);
+        for(auto &row : dp) fill(row.begin(),row.end(),0);
+
+        dp[0][0] = triangle[0][0];
+
+        for(int i = 1;i<n;i++) {
+            int m = triangle[i].size();
+            for(int j = 0;j<m;j++) {
+                
+                
+                int cur = INT_MAX;
+                if(j-1>=0)  cur = dp[i-1][j-1]+triangle[i][j];
+                
+                if(j!=i) cur = min(cur,dp[i-1][j]+triangle[i][j]);
+
+                dp[i][j] = cur;
+                
+               
+            }
+        }
+
+        int ans = INT_MAX;
+
+        for(int i =0;i<dp[n-1].size();i++) {
+            ans = min(ans,dp[n-1][i]);
+        }
+
+        return ans;
+
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
 
-        return recurSol(triangle);
+        //return recurSol(triangle);
+        return tabulation(triangle);
         
     }
 };
