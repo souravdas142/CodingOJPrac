1ccfc8ae0fa758c5d8242d1c37d207ff954a00f0
Sourav Das <souravdas142@gmail.com>
2025-08-13 12:10:44 +0530

    Time: 4 ms (20.45%), Space: 15.1 MB (38.91%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -3,37 +3,30 @@
 
 class Solution {
 public:
-    int recurSolve(vvi& matrix, int i, int j, int n, int m, vvi& dp) {
-        if (i >= n) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        int mini = recurSolve(matrix, i+1, j, n, m, dp);
-        if (j-1 >= 0) mini = min(mini, recurSolve(matrix, i+1, j-1, n, m, dp));
-        if (j+1 < m) mini = min(mini, recurSolve(matrix, i+1, j+1, n, m, dp));
-        
-        return dp[i][j] = mini + matrix[i][j];
+    int recurSolve(vvi& matrix,int i, int j, int n, int m,vvi& dp) {
+        if(i>=n) {
+            return 0;
+        }
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int mini = INT_MAX;
+        if(j-1>=0) mini = min(mini,recurSolve(matrix,i+1,j-1,n,m,dp)+matrix[i][j]);
+        if(j+1<m) mini = min(mini,recurSolve(matrix,i+1,j+1,n,m,dp)+matrix[i][j]);
+        mini = min(mini,recurSolve(matrix,i+1,j,n,m,dp)+matrix[i][j]);
+        return dp[i][j] =  mini;
     }
-    
     int recurSol(vvi& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        // Early return for single row case
-        if (n == 1) {
-            return *min_element(matrix[0].begin(), matrix[0].end());
-        }
-        
-        vvi dp(n, vi(m, -1));
         int ans = INT_MAX;
-        
-        for (int j = 0; j < m; j++) {
-            ans = min(ans, recurSolve(matrix, 0, j, n, m, dp));
+        vvi dp(n,vi(m,INT_MAX));
+        for(int i = 0;i<m;i++) {
+            
+            ans = min(ans,recurSolve(matrix,0,i,n,m,dp));
         }
-        
         return ans;
     }
-    
     int minFallingPathSum(vector<vector<int>>& matrix) {
         return recurSol(matrix);
+        
     }
 };
