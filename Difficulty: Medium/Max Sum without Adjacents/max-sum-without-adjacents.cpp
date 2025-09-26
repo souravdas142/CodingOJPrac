654f061d6bf327e73a081a07d68aa43521c23e35
Sourav Das <souravdas142@gmail.com>
2025-09-26 15:28:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -2,18 +2,31 @@
 class Solution {
   public:
     // calculate the maximum sum with out adjacent
-    int recurSol(vector<int>& arr, int index,vector<int>& dp) {
-        if(index<0) return 0;
-        if(dp[index]!=0) return dp[index];
-        int left = recurSol(arr, index-2,dp) + arr[index];
-        int right = recurSol(arr,index-1,dp);
-        return dp[index] = max(left,right);
+    int recurSol(vector<int>& nums, int i) {
+        if(i<0) return 0;
+        int x = nums[i]+recurSol(nums,i-2);
+        int y = 0+recurSol(nums,i-1);
+        return max(x,y);
+        
+    }
+    int memoSol(vector<int>& nums, int i, vector<int>& dp) {
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int x = nums[i]+memoSol(nums,i-2,dp);
+        int y = 0+memoSol(nums,i-1,dp);
+        return dp[i] = max(x,y);
+        
+        
     }
     int findMaxSum(vector<int>& arr) {
         // code here
+        // int n = arr.size();
+        // int index = n-1;
+        // return recurSol(arr,index);
         int n = arr.size();
+        vector<int> dp(n+1,-1);
         int index = n-1;
-        vector<int> dp(n,0);
-        return recurSol(arr,index,dp);
+        
+        return memoSol(arr,index,dp);
     }
 };
