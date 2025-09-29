0dd7fea9b6bfb7dcb990cdb94b8e9e933cb98ed1
Sourav Das <souravdas142@gmail.com>
2025-09-29 17:24:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
@@ -1,93 +1,34 @@
-
-#define vi vector<int>
-#define vvi vector<vector<int>>
-
 class Solution {
   public:
-    vi dirx = {0,1};
-    vi diry = {1,0};
-    int recurSolve(vvi& grid, int r, int c,vvi& dp) {
-        int n = grid.size();
-        int m = grid[0].size();
-        if(r == n-1 && c == m-1) {
-            return 1;
-        }
-        if(dp[r][c]!=-1) return dp[r][c];
-        int ans = 0;
-        for(int i  = 0;i<2;i++) {
-            int newx = r+dirx[i];
-            int newy = c+diry[i];
-            if(newx<n && newy<m && grid[newx][newy]!=1) {
-                ans += recurSolve(grid,newx,newy,dp);
-            }
-            
-        }
-        return dp[r][c] = ans;
-    }
-    int recurSol(vvi& grid) {
-        if(grid[0][0]==1) return 0;
-        int n = grid.size();
-        int m = grid[0].size();
+    int recurSol(vector<vector<int>>& grid, int m, int n) {
         
-        vvi dp(n,vi(m,-1));
-        return recurSolve(grid,0,0,dp);
+        if(m<0 || n<0 || grid[m][n]==1) return 0;
+        if(m==0 && n==0) return 1;
+        int x = recurSol(grid,m-1,n);
+        int y = recurSol(grid,m,n-1);
+        return x+y;
     }
-    int tabulation(vvi& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vvi dp(n,vi(m,0));
-        if(grid[0][0]==1) return 0;
-        dp[0][0] = 1;
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                if(i == 0 && j==0) continue;
-                if(grid[i][j]==1) continue;
-                int left = (i-1>=0 && grid[i-1][j]!=1)?dp[i-1][j]:0;
-                int right = (j-1>=0 && grid[i][j-1]!=1)?dp[i][j-1]:0;
-                dp[i][j] = left+right;
-            }
-        }
-        return dp[n-1][m-1];
-    }
-    int spaceOpt(vvi& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        if(grid[0][0]==1) return 0;
-        vi dp(m,1);
-        int way = 1;
-        for(int i = 0;i<m;i++) {
-            if(grid[0][i]==1) way = 0;
-            dp[i] = way;
-        }
+    int memoSol(vector<vector<int>>& grid, int m, int n, vector<vector<int>>& dp) {
+        if(m<0 || n<0 || grid[m][n]==1) return 0;
         
-        for(int i =1;i<n;i++) {
-            int prev = 0;
-            vi temp(m,0);
-            for(int j = 0;j<m;j++) {
-                if(grid[i][j]==1) {
-                    int cur = 0;
-                    temp[j] = cur;
-                    prev = cur;
-                    continue;
-                }
-                int cur = 0;
-                if(dp[j]==0 && j>0 && prev==0) cur = 0;
-                else if(dp[j]==0 && !(j>0 && prev==0)) cur = prev;
-                else if(dp[j]!=0 && (j>0 && prev==0)) cur = dp[j];
-                else cur = prev+dp[j];
-                prev = cur;
-                temp[j] = cur;
-            }
-            dp = temp;
-        }
-        return dp[m-1];
+        if(dp[m][n]!=-1) return dp[m][n];
         
+        if(m==0 && n==0) return dp[m][n] = 1;
+        
+        
+        
+        int x = memoSol(grid,m-1,n,dp);
+        int y = memoSol(grid,m,n-1,dp);
+        return dp[m][n] = x+y;
     }
     int uniquePaths(vector<vector<int>> &grid) {
         // code here
-        //return recurSol(grid);
-        //return tabulation(grid);
-        return spaceOpt(grid);
+        int m = grid.size();
+        int n = grid[0].size();
         
+        // return recurSol(grid,m-1,n-1);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return memoSol(grid,m-1,n-1,dp);
+
     }
 };
