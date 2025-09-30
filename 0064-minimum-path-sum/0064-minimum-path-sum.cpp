b142c69ef8c9252cddf0f68985e343ae68e280c9
Sourav Das <souravdas142@gmail.com>
2025-09-30 23:54:55 +0530

    Time: 2 ms (37.78%), Space: 16.2 MB (61.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,12 +20,32 @@ public:
         return min(x,y);
 
     }
+
+    int memoSol(vvi& grid, int n, int m,vvi& dp) {
+    
+        if(n==0 && m==0) return dp[n][m] = grid[n][m];
+
+        int x = INT_MAX;
+        int y = INT_MAX;
+
+        if(dp[n][m]!=-1) return dp[n][m];
+
+        if(n-1>=0)
+            x = grid[n][m] + memoSol(grid,n-1,m,dp);
+        if(m-1>=0)
+            y = grid[n][m] + memoSol(grid,n,m-1,dp);
+
+        return dp[n][m] = min(x,y);
+
+    }
     int minPathSum(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
 
-        return recurSol(grid, n-1, m-1);
+        // return recurSol(grid, n-1, m-1);
+        vvi dp(n,vi(m,-1));
+        return memoSol(grid,n-1,m-1,dp);
         
     }
 };
