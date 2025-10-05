bf70375a1fb00d6ba9c3975b068aa7ec5412e2f0
Sourav Das <souravdas142@gmail.com>
2025-10-05 21:54:00 +0530

    Time: 0 ms (100%), Space: 15 MB (61.56%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -29,6 +29,32 @@ public:
 
         return dp[row][col] = (matrix[row][col]+ans);
     }
+
+    int tabuSol(vvi& matrix, int n) {
+        vvi dp(n,vi(n,0));
+        for(int i = 0;i<n;i++) {
+            dp[0][i] = matrix[0][i];
+        }
+        for(int i = 1;i<n;i++) {
+            for(int j = 0;j<n;j++) {
+                int x = dp[i-1][j];
+                int y = INT_MAX;
+                int z = INT_MAX;
+                if(j-1>=0) y = dp[i-1][j-1];
+                if(j+1<n) z = dp[i-1][j+1];
+
+                int ans = min(x,min(y,z));
+                dp[i][j] = ans+matrix[i][j];
+            }
+        }
+    
+        int ans = INT_MAX;
+        for(int i = 0;i<n;i++) {
+            ans = min(ans,dp[n-1][i]);
+        }
+        return ans;
+    }
+
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int row = n-1;
@@ -39,12 +65,14 @@ public:
         //     ans = min(ans,recurSol(matrix, n, row, col-i));
         // }
 
-        vvi dp(n,vi(n,INT_MAX));
+        // vvi dp(n,vi(n,INT_MAX));
 
-        for(int i = 0;i<n;i++) {
-            ans = min(ans,memoSol(matrix, n, row, col-i,dp));
-        }  
+        // for(int i = 0;i<n;i++) {
+        //     ans = min(ans,memoSol(matrix, n, row, col-i,dp));
+        // }  
 
-        return ans;
+        // return ans;
+
+        return tabuSol(matrix,n);
     }
 };
