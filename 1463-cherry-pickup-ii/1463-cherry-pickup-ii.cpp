5c1fe9ba1f29fc0856c434ba9bc010182c50620a
Sourav Das <souravdas142@gmail.com>
2025-10-06 16:25:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,44 +1,64 @@
-#define vi vector<int>
+
+#define vi vector<int> 
 #define vvi vector<vi>
 
 class Solution {
 public:
-    vi dirx = {-1,0,1};
- 
-    int roboRecurSolve(vvi& grid, int n, int m, int index, int robo1, int robo2) {
 
-
-        if((robo1<0 || robo1>=m) || (robo2<0 || robo2>=m)) return -1e8;
-
-        if(index==n-1) {
-            if(robo1==robo2) return grid[index][robo1];
-            return grid[index][robo1]+grid[index][robo2];
+    int recurSol(vvi& grid, int n, int m, int robo1, int robo2, int i) {
+        if(i>=n || robo1<0 || robo1>=m || robo2<0 || robo2>=m)  return 0;
+        if(i==n-1) {
+            int x = 0;
+            int y = 0;
+            if(robo1>=0 && robo1<m) x = grid[i][robo1];
+            if(robo2>=0 && robo2<m && robo1!=robo2) y = grid[i][robo2];
+            return x+y;
         }
 
-        
+        int move1 = 0, move2 = 0, move3 = 0;
 
-        int maxi = INT_MIN;
+        if(robo1-1>=0) {
+            int x = (robo2-1>=0)?(recurSol(grid,n,m,robo1-1,robo2-1,i+1)):0;
+            int y = (robo2+1<m)?(recurSol(grid,n,m,robo1-1,robo2+1,i+1)):0;
+            int z = recurSol(grid, n,m, robo1-1,robo2,i+1);
+            int ans = max(x,max(y,z));
+            if(robo1!=robo2) ans+=(grid[i][robo1]+grid[i][robo2]);
+            else ans+=(grid[i][robo1]);
+            move1 = ans;
 
-        for(int i = 0;i<3;i++) {
-            for(int j = 0;j<3;j++) {
-                if(robo1 == robo2) {
-                    maxi = max(maxi,grid[index][robo1]+roboRecurSolve(grid,n,m,index+1,robo1+dirx[i],robo2+dirx[j]));
-                }
-                else {
-                    maxi = max(maxi,grid[index][robo1]+grid[index][robo2]+roboRecurSolve(grid,n,m,index+1,robo1+dirx[i],robo2+dirx[j]));
-                }
-            }
         }
-        return maxi;
-    }
-    int recurSol(vvi& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int index = 0;
-        int rob1 = 0, rob2 = m-1;
-        return roboRecurSolve(grid,n,m,index,rob1, rob2);
+
+        if(robo1+1<m) {
+            int x = (robo2-1>=0)?(recurSol(grid,n,m,robo1+1,robo2-1,i+1)):0;
+            int y = (robo2+1<m)?(recurSol(grid,n,m,robo1+1,robo2+1,i+1)):0;
+            int z = recurSol(grid, n,m, robo1+1,robo2,i+1);
+            int ans = max(x,max(y,z));
+            if(robo1!=robo2) ans+=(grid[i][robo1]+grid[i][robo2]);
+            else ans+=(grid[i][robo1]);
+            move2 = ans;
+        }
+
+        if(robo1>=0 && robo1<m) {
+            int x = (robo2-1>=0)?(recurSol(grid,n,m,robo1,robo2-1,i+1)):0;
+            int y = (robo2+1<m)?(recurSol(grid,n,m,robo1,robo2+1,i+1)):0;
+            int z = recurSol(grid, n,m, robo1,robo2,i+1);
+            int ans = max(x,max(y,z));
+            if(robo1!=robo2) ans+=(grid[i][robo1]+grid[i][robo2]);
+            else ans+=(grid[i][robo1]);
+            move3 = ans;
+        }
+
+        return max(move1,max(move2,move3));
+
+
+
+
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        return recurSol(grid);
+        int n = grid.size();
+        int m = grid[0].size();
+        int robo1 = 0, robo2 = m-1;
+        int i = 0;
+        return recurSol(grid,n,m, robo1, robo2, i);
     }
 };
