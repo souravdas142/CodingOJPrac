34e541697e4f164a5aca43d9bd21be75f17b3c4a
Sourav Das <souravdas142@gmail.com>
2025-11-10 14:48:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -1,10 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     // Complete this function
@@ -12,61 +5,26 @@ class Solution {
     bool subArrayExists(vector<int>& arr) {
         // Your code here
         int n = arr.size();
+        int j = 0;
         int i = 0;
         int sum = 0;
+        unordered_map<int,bool> ump;
         int target = 0;
-        unordered_map<int,int> look;
-        
-        while(i<n) {
-            sum+=arr[i];
-            if(sum==target) {
+       
+        while(j<n) {
+            sum+=arr[j];
+            if(sum == 0 ) return true;
+            
+            int need = sum-target;
+            if(ump.find(need)!=ump.end()) {
                 return true;
             }
             
-            int need = sum - target;
-            if(look.find(need)!=look.end()) {
-                return true;
-            }
-            
-            if(look.find(sum)==look.end()) {
-                look.insert({sum,i});
-            }
-            
-            i++;
-            
+            ump[sum] = true;
+            j++;
             
         }
         
         return false;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution obj;
-        if (obj.subArrayExists(arr))
-            cout << "true\n";
-        else
-            cout << "false\n";
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
