90df99dd09a24d161582f60d9a1631ec4dd8a73f
Sourav Das <souravdas142@gmail.com>
2025-12-01 19:42:46 +0530

    Time: 12 ms (31.18%), Space: 23 MB (44.64%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,41 +1,31 @@
 class Solution {
-
 public:
-    bool check_possible(int mid, vector<int>& piles, int h) {
-        int n = piles.size();
-        int i = 0;
-        long long cnt=0;
-        while(i<n) {
-            long long time = ceil((double)piles[i] / mid);
-
-            //cout<<"piles["<<i<<"] = "<<piles[i]<<" / "<<mid<<" = "<<time<<endl;
-            cnt+=time;
-            i++;
+    bool check(int & ans, vector<int>& piles, int& h) {
+        int maybe = 0;
+        for(int &e: piles) {
+            int x = ceil(((double)e)/((double)ans));
+            maybe+=x;
+            if(maybe>h) return false;
         }
-        if(cnt>h) return false;
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-
         int n = piles.size();
-        int high = 0;
-        for(auto e:piles) {
-            high = max(high,e);
-        }
         int low = 1;
+        int high = piles[0];
+        for(auto& e: piles) high = max(e,high);
 
         while(low<high) {
-            int p_ans = low+(high-low)/2;
-            
-            if(check_possible(p_ans,piles,h)) {
-                high = p_ans;
+            int mid = low+(high-low)/2;
+            if(check(mid,piles,h)) {
+                high = mid;
             }
             else {
-                low = p_ans+1;
+                low = mid+1;
             }
         }
 
         return high;
-        
+
     }
 };
