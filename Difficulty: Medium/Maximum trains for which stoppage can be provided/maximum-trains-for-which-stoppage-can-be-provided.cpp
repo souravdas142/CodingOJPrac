964d0dd8f67485561fc3230d35774f775525a524
Sourav Das <souravdas142@gmail.com>
2025-12-22 23:11:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum trains for which stoppage can be provided/maximum-trains-for-which-stoppage-can-be-provided.cpp
+++ Difficulty: Medium/Maximum trains for which stoppage can be provided/maximum-trains-for-which-stoppage-can-be-provided.cpp
@@ -1,58 +1,51 @@
-
-void printVector(vector<pair<int,int>>& p) {
-    for(auto &e: p) {
-        cout<<e.first<<" "<<e.second<<endl;
-    }
-    
-}
-
 class Solution {
   public:
-    int solve(vector<pair<int,int>>& trains) {
-        if (trains.empty()) return 0;
-        sort(trains.begin(),trains.end(),[](auto& a, auto& b)->bool {
-            return a.second<b.second;
+  
+    int solve(vector<pair<int,int>>& train) {
+        
+        int ans = 0;
+        
+        int n = train.size();
+        sort(train.begin(),train.end(),[](auto& a, auto& b)->bool{
+            
+           if(a.second == b.second) return a.first > b.first;
+           return a.second < b.second;
+            
         });
-        int n = trains.size();
-       
         
+        int start = train[0].first, end = train[0].second;
+        ans = 1;
         
-        
-        
-        int arr = trains[0].first, dep = trains[0].second;
-        int ans = 1;
-        int j = 1;
-        
-        while(j<n) {
-            if(trains[j].first>=dep) {
+        for(int i = 1;i<n; i++) {
+            
+            if(train[i].first >= end) {
+                start = train[i].first;
+                end = train[i].second;
                 ans++;
-                arr = trains[j].first;
-                dep = trains[j].second;
             }
-            j++;
+            
         }
-        //printVector(trains);
-        // cout<<ans<<endl;
+        
+        
+        
+        
         return ans;
     }
-    
+  
     int maxStop(int train, int plat, vector<vector<int>> &trains) {
         // code here
-        int n = trains.size();
-        int m = plat;
-        unordered_map<int,vector<pair<int,int>>> trains2;
+        unordered_map<int,vector<pair<int,int>>> tsch;
         
-        for(auto& train: trains) {
-            trains2[train[2]].push_back({train[0],train[1]});
+        int n = trains.size();
+        for(int i = 0;i<n;i++) {
+            tsch[trains[i][2]].push_back({trains[i][0],trains[i][1]});
         }
         
         int ans = 0;
         
-        
-        for(auto& pr: trains2) {
-            ans+=solve(pr.second);
+        for(auto& pf: tsch) {
+            ans += solve(pf.second);
         }
-        
         return ans;
     }
 };
