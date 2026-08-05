9f13a815674cee35e773d37cfd5962f27f8814f2
Sourav Das <souravdas142@gmail.com>
2026-08-05 15:15:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Paths from Top Left to Bottom Right/count-paths-from-top-left-to-bottom-right.java
+++ Difficulty: Medium/Count Paths from Top Left to Bottom Right/count-paths-from-top-left-to-bottom-right.java
@@ -1,27 +1,41 @@
 class Solution {
-    public int solve(int m, int n, int[][] dp) {
-        if(m<0 || n<0) return 0;
+    
+    private int solve(int m, int n) {
+       
         
-        if(m==0 && n== 0) return 1;
+        if(m<0 || n<0) return 0;
+        if(m==0 && n==0) return 1;
+        int up = solve(m-1,n);
+        int left = solve(m,n-1);
+        
+        return (left+up);
+    }
+    
+    private int solve2(int m, int n,int[][] dp) {
+       
+        
+        if(m<0 || n<0) return 0;
+        if(m==0 && n==0) return 1;
         
         if(dp[m][n]!=0) return dp[m][n];
         
-        int left = solve(m-1,n,dp);
-        int right = solve(m,n-1,dp);
+        int up = solve2(m-1,n,dp);
+        int left = solve2(m,n-1,dp);
         
-        return dp[m][n] = left+right;
+        return dp[m][n] = (left+up);
     }
-
     
     public int numberOfPaths(int m, int n) {
         // code here
         
-        int ans = 0;
-        int[][] dp = new int[m][n];
-        ans = solve(m-1,n-1,dp);
         
-       
-        return ans;
+        int[][] dp = new int[m][n];
+        // return solve(m-1,n-1);
+        
+        return solve2(m-1,n-1,dp);
+        
+        
+        
         
     }
 }
