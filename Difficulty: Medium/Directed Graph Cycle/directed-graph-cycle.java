8be1c9306ef0e972a2a0c6f2238adcdfd7fc8592
Sourav Das <souravdas142@gmail.com>
2026-08-25 11:08:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,52 +1,46 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
-        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
         int n = edges.length;
-        boolean[] visited = new boolean[V];
+        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
+        int[] indeg = new int[V];
+        // boolean[] visited = new boolean[V];
         
         for(int i = 0;i<V;i++) adjlist.add(new ArrayList<Integer>());
+        
+        
+        
         for(int i = 0;i<n;i++) {
             int u = edges[i][0];
             int v = edges[i][1];
-            
             adjlist.get(u).add(v);
+            indeg[v]++;
         }
-        Set<Integer> path = new HashSet<>();
+        
+        Queue<Integer> que = new ArrayDeque<Integer>();
+        
         for(int i = 0;i<V;i++) {
-            if(visited[i]==false) {
-                
-                boolean status = dfs(i,adjlist,visited,path);
-                path.clear();
-                if(status == true) {
-                    ///System.out.println(i);
-                    return true;
+            if(indeg[i]==0) {
+                que.add(i);
+                // visited[i]=true;
+            }
+        }
+        
+        int ans = 0;
+        
+        while(!que.isEmpty()) {
+            int u = que.poll();
+            ans++;
+            for(Integer v: adjlist.get(u)) {
+                indeg[v]--;
+                if(indeg[v]==0) {
+                    que.add(v);
+                    // visited[v]=true;
                 }
-                
             }
         }
-        return false;
-    }
-    
-    public boolean dfs(int u, ArrayList<ArrayList<Integer>> adjlist, 
-                        boolean[] visited, Set<Integer> path) {
         
-        if(visited[u]==true) {
-            if(path.contains(u)==true) {
-                return true;
-            }
-            return false;
-        }
-        path.add(u); 
-        visited[u] = true;
-        
-        for(Integer v: adjlist.get(u)) {
-            if(dfs(v, adjlist,visited,path)==true) {
-                
-                return true;
-            }
-        }
-        path.remove(u);
-        return false;
+        if(ans==V) return false;
+        return true;
     }
 }
