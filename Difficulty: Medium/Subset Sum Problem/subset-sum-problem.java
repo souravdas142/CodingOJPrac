c4baeca86d4795943859ec99100e89d0c4143b67
Sourav Das <souravdas142@gmail.com>
2026-09-01 23:22:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,56 @@
+class Solution {
+    public static boolean solve1(int[] arr, int index, int target) {
+        
+        if(target==0) return true;
+        if(index==0) return arr[index]==target;
+        
+        
+        // not take
+        boolean not_take = solve1(arr,index-1,target);
+        if(not_take==true) return true;
+        
+        // take
+        
+        boolean take = false;
+        if(target >= arr[index])
+            take = solve1(arr,index-1,target-arr[index]);
+        
+        return (not_take || take);
+    }
+    
+    public static boolean solve2(int[] arr, int index, int target,int[][] dp) {
+        
+        if(target==0) return true;
+        if(index==0) return arr[index]==target;
+        
+        
+        
+        if(dp[index][target]!=-1) return dp[index][target]==1;
+        // not take
+        boolean not_take = solve2(arr,index-1,target,dp);
+        if(not_take==true) {
+            dp[index][target] = 1;
+            return true;
+        }
+        
+        // take
+        
+        boolean take = false;
+        if(target >= arr[index])
+            take = solve2(arr,index-1,target-arr[index],dp);
+        
+        dp[index][target] = ((not_take || take)==true)?1:0;
+        return (not_take || take);
+    }
+    
+    static boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n = arr.length;
+        int index = n-1;
+        
+        // return solve1(arr,index,sum);
+        int[][] dp = new int[n][sum+1];
+        for(int i = 0;i<n;i++) Arrays.fill(dp[i],-1);
+        return solve2(arr,index,sum,dp);
+    }
+}
