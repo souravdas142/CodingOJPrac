cd7a7142f387afd981803864e9f7cbd2329d2aba
Sourav Das <souravdas142@gmail.com>
2026-09-05 23:12:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.java
+++ Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.java
@@ -62,11 +62,60 @@ class Solution {
         
     }
     
+    
+    public static int solve2(int[] arr, int k) {
+        int n = arr.length;
+        
+        int[] dp = new int[k+1];
+        int[] temp = new int[k+1];
+      
+        // for(int i = 0;i<n;i++) {
+        //     dp[i][0] = 1;
+            
+        // }
+        
+        // // if(arr[0]<=k) dp[0][arr[0]] = 1;
+        // // if(arr[0]<=k && arr[0]==0) dp[0][arr[0]] = 2;
+        
+        // //dp[0][0]=1;
+        // if (arr[0] == 0) dp[0][0] = 2; 
+        // if (arr[0] <= k) dp[0][arr[0]] = 1; 
+          
+      
+        if (arr[0] == 0) {
+            dp[0] = 2; // target == 0 and arr[0] == 0 -> 2 ways
+        } else {
+            dp[0] = 1; // target == 0 and arr[0] != 0 -> 1 way (empty set)
+            if (arr[0] <= k) {
+                dp[arr[0]] = 1; // target == arr[0] -> 1 way
+            }
+        }
+        
+        for(int index = 1;index<n;index++) {
+            for(int target= 0;target<=k;target++) {
+                int notTake = dp[target];
+        
+                int take = 0;
+                if(target>=arr[index])
+                    take = dp[target-arr[index]];
+                
+                temp[target] = take+notTake;
+            }
+            int[] swap = dp;
+            dp = temp;
+            temp = swap;
+            
+        }
+        
+        return dp[k];
+        
+    }
+    
     static int perfectSum(int[] arr, int target) {
         // code here
         int n = arr.length;
         int index = n-1;
-        int ans = solve1(arr,target);
+        int ans = solve2(arr,target);
         return ans;
     }
 }
